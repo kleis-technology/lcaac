48a488a4ce2629d4caadaeb5798a4ce9099fa9e2
GitHub Action <action@github.com>
2022-07-13 19:58:40 +0000

    Template cleanup

--- src/main/kotlin/com/github/albanseurat/lcaplugin/services/MyApplicationService.kt
+++ src/main/kotlin/com/github/albanseurat/lcaplugin/services/MyApplicationService.kt
@@ -1,6 +1,6 @@
-package org.jetbrains.plugins.template.services
+package com.github.albanseurat.lcaplugin.services
 
-import org.jetbrains.plugins.template.MyBundle
+import com.github.albanseurat.lcaplugin.MyBundle
 
 class MyApplicationService {
 
--- src/main/kotlin/com/github/albanseurat/lcaplugin/services/MyProjectService.kt
+++ src/main/kotlin/com/github/albanseurat/lcaplugin/services/MyProjectService.kt
@@ -1,7 +1,7 @@
-package org.jetbrains.plugins.template.services
+package com.github.albanseurat.lcaplugin.services
 
 import com.intellij.openapi.project.Project
-import org.jetbrains.plugins.template.MyBundle
+import com.github.albanseurat.lcaplugin.MyBundle
 
 class MyProjectService(project: Project) {
 
