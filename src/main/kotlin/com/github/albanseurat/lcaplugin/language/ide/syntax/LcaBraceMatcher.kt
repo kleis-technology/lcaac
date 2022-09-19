974aa7ae9cbb2cd4561c4b54db92de9576b51fcc
Alban Seurat <alban.seurat@me.com>
2022-09-19 18:21:58 +0200

    automatic brace completion

---
+++ src/main/kotlin/com/github/albanseurat/lcaplugin/language/ide/syntax/LcaBraceMatcher.kt
@@ -0,0 +1,23 @@
+package com.github.albanseurat.lcaplugin.language.ide.syntax
+
+import com.github.albanseurat.lcaplugin.psi.LcaTypes
+import com.intellij.json.JsonElementTypes
+import com.intellij.lang.BracePair
+import com.intellij.lang.PairedBraceMatcher
+import com.intellij.psi.PsiFile
+import com.intellij.psi.tree.IElementType
+
+class LcaBraceMatcher : PairedBraceMatcher {
+
+    override fun getPairs(): Array<BracePair> {
+        return arrayOf(BracePair(LcaTypes.LBRACE, LcaTypes.RBRACE, true))
+    }
+
+    override fun isPairedBracesAllowedBeforeType(lbraceType: IElementType, contextType: IElementType?): Boolean {
+        return true
+    }
+
+    override fun getCodeConstructStart(file: PsiFile?, openingBraceOffset: Int): Int {
+        return openingBraceOffset;
+    }
+}
