6762f83c36a42fcf43d01caf87d7156c8075d77f
Peva Blanchard <pblanchard@kleis.ch>
2022-10-18 08:52:10 +0200

    scsv: import scsv with element creator

--- src/main/kotlin/com/github/albanseurat/lcaplugin/actions/ImportScsvFileAction.kt
+++ src/main/kotlin/com/github/albanseurat/lcaplugin/actions/ImportScsvFileAction.kt
@@ -1,24 +1,54 @@
 package com.github.albanseurat.lcaplugin.actions
 
+import com.intellij.ide.IdeBundle
+import com.intellij.ide.actions.ElementCreator
+import com.intellij.internal.statistic.collectors.fus.fileTypes.FileTypeUsageCounterCollector
 import com.intellij.openapi.actionSystem.AnAction
 import com.intellij.openapi.actionSystem.AnActionEvent
-import com.intellij.openapi.actionSystem.CommonDataKeys
+import com.intellij.openapi.actionSystem.LangDataKeys
+import com.intellij.openapi.application.WriteAction
 import com.intellij.openapi.fileChooser.FileChooserDescriptorFactory
 import com.intellij.openapi.fileChooser.FileChooserDialog
 import com.intellij.openapi.fileChooser.ex.FileChooserDialogImpl
-import com.intellij.openapi.progress.ProgressIndicatorProvider
+import com.intellij.openapi.progress.ProgressManager
+import com.intellij.openapi.project.Project
+import com.intellij.psi.PsiDirectory
+import com.intellij.psi.PsiElement
+import com.intellij.psi.PsiFile
+import java.io.File
 
-class ImportScsvFileAction(): AnAction() {
-
+class ImportScsvFileAction : AnAction() {
 
     override fun actionPerformed(e: AnActionEvent) {
-        val dataContext = e.dataContext
-        val project = CommonDataKeys.PROJECT.getData(dataContext) ?: return
+        val view = e.getData(LangDataKeys.IDE_VIEW) ?: return
+        val project = e.project ?: return
+        val dir = view.orChooseDirectory ?: return
+
+        val elementCreator = MyElementCreator(project, dir, "error")
+        val elements = elementCreator.tryCreate("scsv.lca")
+        val containerFile = elements[0]
+
         val descriptor = FileChooserDescriptorFactory.createSingleFileDescriptor("gz")
         val fileChooserDialog: FileChooserDialog = FileChooserDialogImpl(descriptor, project)
-        val file = fileChooserDialog.choose(project)
-        val task = ImportScsvFileBackgroundTask(project, file[0])
-        val progressIndicator = ProgressIndicatorProvider.getGlobalProgressIndicator()
-        task.run(progressIndicator)
+        val scsvFile = fileChooserDialog.choose(project)[0]
+
+        val task = ImportScsvFileBackgroundTask(project, scsvFile, containerFile)
+        ProgressManager.getInstance().run(task)
+    }
+
+    private class MyElementCreator(
+        val project: Project,
+        val directory: PsiDirectory,
+        val errorTitle: String
+    ) : ElementCreator(project, errorTitle) {
+        override fun create(newName: String): Array<PsiElement> {
+            val file = WriteAction.compute<PsiFile, Exception> { directory.createFile(newName) }
+            FileTypeUsageCounterCollector.triggerCreate(project, file.virtualFile)
+            return arrayOf(file)
+        }
+
+        override fun getActionName(newName: String): String {
+            return IdeBundle.message("progress.creating.file", directory.virtualFile.presentableUrl, File.separator, newName)
+        }
     }
 }
--- src/main/kotlin/com/github/albanseurat/lcaplugin/actions/ImportScsvFileBackgroundTask.kt
+++ src/main/kotlin/com/github/albanseurat/lcaplugin/actions/ImportScsvFileBackgroundTask.kt
@@ -1,24 +1,27 @@
 package com.github.albanseurat.lcaplugin.actions
 
 import com.github.albanseurat.lcaplugin.LcaLanguage
+import com.intellij.openapi.command.WriteCommandAction
 import com.intellij.openapi.progress.ProgressIndicator
 import com.intellij.openapi.progress.Task
 import com.intellij.openapi.project.Project
 import com.intellij.openapi.vfs.VirtualFile
+import com.intellij.psi.PsiElement
 import com.intellij.psi.PsiFileFactory
-import com.intellij.psi.impl.PsiFileFactoryImpl
 import org.openlca.simapro.csv.CsvHeader
 import org.openlca.simapro.csv.SimaProCsv
 import java.util.zip.GZIPInputStream
 
-class ImportScsvFileBackgroundTask(project: Project, val file: VirtualFile): Task.Backgroundable(project, "Importing SCSV...")
+class ImportScsvFileBackgroundTask(project: Project, private val scsvFile: VirtualFile, private val containerFile: PsiElement): Task.Backgroundable(project, "Importing SCSV...")
 {
     override fun run(indicator: ProgressIndicator) {
-        val inputStream = GZIPInputStream(file.inputStream)
+        val inputStream = GZIPInputStream(scsvFile.inputStream)
         val reader = SimaProCsv.readerOf(inputStream, SimaProCsv.defaultCharset())
 
         val header = CsvHeader.readFrom(reader)
         val datasets = SimaProCsv.read(header, reader)
+
+        indicator.fraction = 0.0
 
         val it = datasets.processes()[0]
 
@@ -44,7 +47,11 @@ class ImportScsvFileBackgroundTask(project: Project, val file: VirtualFile): Tas
             }
         """.trimIndent()
 
-        val psiFileFactory: PsiFileFactory = PsiFileFactoryImpl(project)
-        psiFileFactory.createFileFromText("import.lca", LcaLanguage.INSTANCE, content)
+        val ds = PsiFileFactory.getInstance(project).createFileFromText(LcaLanguage.INSTANCE, content)
+        WriteCommandAction.writeCommandAction(project).run<RuntimeException> {
+            containerFile.add(ds)
+        }
+
+        indicator.fraction = 1.0
     }
 }
