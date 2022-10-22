70b1b5f83e3b9355571eb82f0a5d92fe712567da
Peva Blanchard <pblanchard@kleis.ch>
2022-10-22 18:10:49 +0200

    stream creation of dataset files, one dataset per file, one category per subdirectory

--- src/main/kotlin/com/github/albanseurat/lcaplugin/actions/ImportScsvFileAction.kt
+++ src/main/kotlin/com/github/albanseurat/lcaplugin/actions/ImportScsvFileAction.kt
@@ -1,10 +1,9 @@
 package com.github.albanseurat.lcaplugin.actions
 
 import com.github.albanseurat.lcaplugin.LcaLanguage
-import com.github.albanseurat.lcaplugin.project.creators.DatasetDirectoryCreator
-import com.github.albanseurat.lcaplugin.project.creators.DatasetFileCreator
 import com.github.albanseurat.lcaplugin.services.ScsvProcessBlockFormatter
 import com.github.albanseurat.lcaplugin.services.ScsvProcessBlockStream
+import com.intellij.internal.statistic.collectors.fus.fileTypes.FileTypeUsageCounterCollector
 import com.intellij.openapi.actionSystem.AnAction
 import com.intellij.openapi.actionSystem.AnActionEvent
 import com.intellij.openapi.actionSystem.LangDataKeys
@@ -16,7 +15,9 @@ import com.intellij.openapi.fileChooser.ex.FileChooserDialogImpl
 import com.intellij.openapi.progress.ProgressIndicator
 import com.intellij.openapi.progress.ProgressManager
 import com.intellij.openapi.progress.Task
-import com.intellij.psi.*
+import com.intellij.psi.PsiDirectory
+import com.intellij.psi.PsiFile
+import com.intellij.psi.PsiFileFactory
 import java.util.zip.GZIPInputStream
 
 class ImportScsvFileAction : AnAction() {
@@ -34,9 +35,9 @@ class ImportScsvFileAction : AnAction() {
         ProgressManager.getInstance().run(object : Task.Backgroundable(project, "Importing SCSV file...") {
             override fun run(indicator: ProgressIndicator) {
                 indicator.fraction = 0.0
-                indicator.text = "Reading ..."
+                indicator.text = "Processing ..."
 
-                val scsvFileMap: HashMap<String, ArrayList<PsiFile>> = HashMap()
+                val subdirectories: HashMap<String, PsiDirectory> = HashMap()
 
                 var processBlockCount = 0
                 ScsvProcessBlockStream {
@@ -45,44 +46,33 @@ class ImportScsvFileAction : AnAction() {
                     val ds = runReadAction {
                         PsiFileFactory.getInstance(project).createFileFromText(LcaLanguage.INSTANCE, content)
                     }
+
                     val key = it.category().name.lowercase()
-                    if (key !in scsvFileMap.keys) {
-                        scsvFileMap[key] = ArrayList()
+                    val subdir: PsiDirectory = subdirectories[key] ?: run {
+                        val createdSubdir = WriteCommandAction.writeCommandAction(project)
+                            .compute<PsiDirectory, Throwable> {
+                                val subdir = dir.createSubdirectory(key)
+                                FileTypeUsageCounterCollector.triggerCreate(project, subdir.virtualFile)
+                                subdir
+                            }
+                        subdirectories[key] = createdSubdir
+                        createdSubdir
                     }
-                    scsvFileMap[key]?.add(ds)
+
+                    val containingFile = WriteCommandAction.writeCommandAction(project)
+                        .compute<PsiFile, Throwable> {
+                            val file = subdir.createFile("${it.identifier()}.lca")
+                            FileTypeUsageCounterCollector.triggerCreate(project, file.virtualFile)
+                            file
+                        }
+
+                    WriteCommandAction.writeCommandAction(containingFile, ds).run<Throwable> {
+                        containingFile.node.addChild(ds.node.firstChildNode)
+                    }
 
                     processBlockCount += 1
-                    indicator.text = "Read $processBlockCount process blocks"
+                    indicator.text = "Processing ... $processBlockCount datasets"
                 }.read(GZIPInputStream(scsvFile.inputStream))
-
-
-                indicator.fraction = 0.0
-                indicator.text = "Writing files..."
-
-                val subDirectoryCreator = DatasetDirectoryCreator(project, dir, "error")
-
-                var datasetsCount = 0
-                scsvFileMap.keys.forEach { key ->
-                    val subdir = WriteCommandAction.writeCommandAction(project).compute<Array<PsiElement>, Throwable> {
-                        subDirectoryCreator.tryCreate(key)
-                    }[0] as PsiDirectory
-                    val fileCreator = DatasetFileCreator(project, subdir, "error")
-                    scsvFileMap[key]?.forEach { ds ->
-                        ProgressManager.checkCanceled()
-                        val elements = WriteCommandAction.writeCommandAction(project)
-                            .compute<Array<PsiElement>, Throwable> {
-                                fileCreator.tryCreate("${datasetsCount}.lca")
-                            }
-                        val containerFile = elements[0].containingFile
-                        WriteCommandAction.writeCommandAction(containerFile, ds).run<Throwable> {
-                            containerFile.node.addChild(ds.node.firstChildNode)
-                            containerFile.node.addLeaf(TokenType.NEW_LINE_INDENT, "\n\n", null)
-                        }
-                        datasetsCount += 1
-                        indicator.fraction = datasetsCount.toDouble() / processBlockCount.toDouble()
-                        indicator.text = "Wrote $datasetsCount/$processBlockCount datasets"
-                    }
-                }
 
                 indicator.fraction = 1.0
                 indicator.text = "Done"
