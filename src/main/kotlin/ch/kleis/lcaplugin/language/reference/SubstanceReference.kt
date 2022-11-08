0ba74bdd37d871433ed7ebccb53cbee6fd862e65
florian-bellotti <florian.bellotti@viacesi.fr>
2022-11-08 16:55:15 +0100

    :sparkles: Introducing product and substance autocomplete

--- src/main/kotlin/ch/kleis/lcaplugin/language/reference/SubstanceReference.kt
+++ src/main/kotlin/ch/kleis/lcaplugin/language/reference/SubstanceReference.kt
@@ -1,12 +1,22 @@
 package ch.kleis.lcaplugin.language.reference
 
 import ch.kleis.lcaplugin.language.SearchTrait
+import ch.kleis.lcaplugin.language.psi.stub.LcaSubIndexKeys.SUBSTANCES
+import com.intellij.codeInsight.lookup.LookupElement
+import com.intellij.codeInsight.lookup.LookupElementBuilder
 import com.intellij.openapi.util.TextRange
-import com.intellij.psi.*
+import com.intellij.psi.PsiElement
+import com.intellij.psi.PsiElementResolveResult
+import com.intellij.psi.PsiNamedElement
+import com.intellij.psi.PsiPolyVariantReference
+import com.intellij.psi.PsiReferenceBase
+import com.intellij.psi.ResolveResult
+import com.intellij.psi.stubs.StubIndex
 
-
-class SubstanceReference(element: PsiNamedElement, textRange: TextRange) : PsiReferenceBase<PsiNamedElement>(element, textRange),
-    SearchTrait, PsiPolyVariantReference {
+class SubstanceReference(
+    element: PsiNamedElement,
+    textRange: TextRange
+) : PsiReferenceBase<PsiNamedElement>(element, textRange), SearchTrait, PsiPolyVariantReference {
 
     private val processIdentifier: String
 
@@ -14,14 +24,18 @@ class SubstanceReference(element: PsiNamedElement, textRange: TextRange) : PsiRe
         processIdentifier = element.name!!
     }
 
+    override fun getVariants(): Array<LookupElement> =
+        StubIndex.getInstance()
+            .getAllKeys(SUBSTANCES, element.project)
+            .map { LookupElementBuilder.create("\"${it.replace(", ","\", \"")}") } // Ugly
+            .toTypedArray()
+
     override fun resolve(): PsiElement? {
         val resolveResults = multiResolve(false)
         return if (resolveResults.size == 1) resolveResults[0].element else null
     }
 
-    override fun multiResolve(incompleteCode: Boolean): Array<ResolveResult> {
-        return this.findSubstances(element.project, processIdentifier)
+    override fun multiResolve(incompleteCode: Boolean): Array<ResolveResult> =
+        this.findSubstances(element.project, processIdentifier)
             .map { PsiElementResolveResult(it) }.toTypedArray()
-    }
-
 }
