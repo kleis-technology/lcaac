96f05d4dfaab8851e4265f78add8bcf6ff8b99e9
Peva Blanchard <pblanchard@kleis.ch>
2022-11-16 08:13:12 +0100

    fixed line marker provider

--- src/main/kotlin/ch/kleis/lcaplugin/language/ide/insight/LcaLineMarkerProvider.kt
+++ src/main/kotlin/ch/kleis/lcaplugin/language/ide/insight/LcaLineMarkerProvider.kt
@@ -1,6 +1,5 @@
 package ch.kleis.lcaplugin.language.ide.insight
 
-import ch.kleis.lcaplugin.LcaIcons
 import ch.kleis.lcaplugin.psi.LcaProcess
 import ch.kleis.lcaplugin.psi.LcaTypes
 import com.intellij.codeInsight.daemon.RelatedItemLineMarkerInfo
@@ -17,10 +16,10 @@ class LcaLineMarkerProvider : RelatedItemLineMarkerProvider() {
     override fun collectNavigationMarkers(
         element: PsiElement, result: MutableCollection<in RelatedItemLineMarkerInfo<*>>
     ) {
-        if(element.elementType == LcaTypes.IDENTIFIER && element.parent is LcaProcess) {
+        if(element.elementType == LcaTypes.STRING_LITERAL && element.parent is LcaProcess) {
             val builder: NavigationGutterIconBuilder<PsiElement> = NavigationGutterIconBuilder.create(AllIcons.Actions.Execute)
                 .setTargets(element)
-                .setTooltipText("Calculate process impact");
+                .setTooltipText("Assess process impact");
             result.add(builder.createLineMarkerInfo(element))
         }
     }
