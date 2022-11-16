bb9cca224f6c54e3e64b93c5bbb5e14e634c00bc
florian-bellotti <florian.bellotti@viacesi.fr>
2022-11-16 15:18:49 +0100

    :sparkles: Adding partially functional findUsages

---
+++ src/main/kotlin/ch/kleis/lcaplugin/language/findUsages/LcaFindUsagesProvider.kt
@@ -0,0 +1,39 @@
+package ch.kleis.lcaplugin.language.findUsages
+
+import ch.kleis.lcaplugin.language.parser.LcaLexerAdapter
+import ch.kleis.lcaplugin.language.psi.type.Product
+import ch.kleis.lcaplugin.language.psi.type.Substance
+import ch.kleis.lcaplugin.psi.LcaTypes.IDENTIFIER
+import ch.kleis.lcaplugin.psi.LcaTypes.STRING_LITERAL
+import com.intellij.lang.cacheBuilder.DefaultWordsScanner
+import com.intellij.lang.cacheBuilder.WordsScanner
+import com.intellij.lang.findUsages.FindUsagesProvider
+import com.intellij.psi.PsiElement
+import com.intellij.psi.PsiNamedElement
+import com.intellij.psi.tree.TokenSet
+
+
+class LcaFindUsagesProvider : FindUsagesProvider {
+
+    override fun getWordsScanner(): WordsScanner =
+        DefaultWordsScanner(
+            LcaLexerAdapter(),
+            TokenSet.create(IDENTIFIER),
+            TokenSet.EMPTY,
+            TokenSet.create(STRING_LITERAL),
+        )
+
+    override fun canFindUsagesFor(psiElement: PsiElement): Boolean =
+        psiElement is PsiNamedElement
+
+    override fun getHelpId(psiElement: PsiElement): String? = null
+
+    override fun getType(element: PsiElement): String =
+        if (element is Product) "Product" else if (element is Substance) "Substance" else ""
+
+    override fun getDescriptiveName(element: PsiElement): String =
+        (element as? PsiNamedElement)?.name.orEmpty()
+
+    override fun getNodeText(element: PsiElement, useFullName: Boolean): String =
+        (element as? PsiNamedElement)?.name.orEmpty()
+}
