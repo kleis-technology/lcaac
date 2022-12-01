1d8d72e59cd7fb60732093034781fd31c391606b
Peva Blanchard <pblanchard@kleis.ch>
2022-12-01 12:07:10 -1000

    replaced Stream.toList()

--- src/main/kotlin/ch/kleis/lcaplugin/services/formatter/TextBlock.kt
+++ src/main/kotlin/ch/kleis/lcaplugin/services/formatter/TextBlock.kt
@@ -4,6 +4,6 @@ data class TextBlock(val regions: List<TextRegion>): TextRegion {
     override fun getLines(): List<String> {
         return regions.stream()
             .flatMap { it.getLines().stream() }
-            .toList()
+            .collect(Collectors.toList())
     }
 }
