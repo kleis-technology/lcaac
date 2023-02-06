30d8ad08edb3358f161b102f9d72406374eae04d
Peva Blanchard <pblanchard@kleis.ch>
2023-02-06 15:47:22 +0100

    ojalgo solver: simplified if statements

--- src/main/kotlin/ch/kleis/lcaplugin/compute/matrix/impl/ojalgo/OjalgoSolver.kt
+++ src/main/kotlin/ch/kleis/lcaplugin/compute/matrix/impl/ojalgo/OjalgoSolver.kt
@@ -3,19 +3,13 @@ package ch.kleis.lcaplugin.compute.matrix.impl.ojalgo
 import ch.kleis.lcaplugin.compute.matrix.impl.Matrix
 import ch.kleis.lcaplugin.compute.matrix.impl.Solver
 import org.ojalgo.RecoverableCondition
-
-import org.ojalgo.matrix.task.SolverTask;
+import org.ojalgo.matrix.task.SolverTask
 
 class OjalgoSolver : Solver {
     override fun solve(lhs: Matrix, rhs: Matrix): Matrix? {
         val a = lhs as OjalgoMatrix
         val b = rhs as OjalgoMatrix
         if (a.rowDim() == 0 || b.colDim() == 0) {
-            return OjalgoMatrixFactory().zero(a.rowDim(), b.colDim())
-        }
-            return OjalgoMatrixFactory().zero(0, b.colDim())
-        }
-        if (b.colDim() == 0) {
             return OjalgoMatrixFactory().zero(a.rowDim(), b.colDim())
         }
         return try {
