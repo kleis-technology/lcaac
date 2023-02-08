e6f1a6d3cc9cb1dfe0287843794d6b4d188beb9b
Peva Blanchard <pblanchard@kleis.ch>
2023-02-08 01:13:05 +0100

    refined usage types

--- src/main/kotlin/ch/kleis/lcaplugin/language/find_usages/LcaUsageType.kt
+++ src/main/kotlin/ch/kleis/lcaplugin/language/find_usages/LcaUsageType.kt
@@ -4,8 +4,9 @@ import com.intellij.usages.impl.rules.UsageType
 
 class LcaUsageType {
     companion object {
-        val INPUT = UsageType { "Technosphere - Input" }
-        val PRODUCT = UsageType { "Technosphere - Product" }
-        val BIO_EXCHANGE = UsageType { "Biosphere"}
+        val TECHNOSPHERE_INPUT = UsageType { "Technosphere - Input" }
+        val TECHNOSPHERE_PRODUCT = UsageType { "Technosphere - Product" }
+        val BIOSPHERE = UsageType { "Biosphere"}
+        val SUBSTANCE = UsageType { "Substance" }
     }
 }
--- src/main/kotlin/ch/kleis/lcaplugin/language/find_usages/LcaUsageTypeProvider.kt
+++ src/main/kotlin/ch/kleis/lcaplugin/language/find_usages/LcaUsageTypeProvider.kt
@@ -3,6 +3,7 @@ package ch.kleis.lcaplugin.language.find_usages
 import ch.kleis.lcaplugin.language.psi.type.PsiBioExchange
 import ch.kleis.lcaplugin.language.psi.type.PsiInputExchange
 import ch.kleis.lcaplugin.language.psi.type.PsiProductExchange
+import ch.kleis.lcaplugin.language.psi.type.PsiSubstance
 import com.intellij.psi.PsiElement
 import com.intellij.usages.impl.rules.UsageType
 import com.intellij.usages.impl.rules.UsageTypeProvider
@@ -10,13 +11,16 @@ import com.intellij.usages.impl.rules.UsageTypeProvider
 class LcaUsageTypeProvider : UsageTypeProvider {
     override fun getUsageType(element: PsiElement): UsageType? {
         if (element is PsiInputExchange) {
-            return LcaUsageType.INPUT
+            return LcaUsageType.TECHNOSPHERE_INPUT
         }
         if (element is PsiProductExchange) {
-            return LcaUsageType.PRODUCT
+            return LcaUsageType.TECHNOSPHERE_PRODUCT
         }
         if (element is PsiBioExchange) {
-            return LcaUsageType.BIO_EXCHANGE
+            return LcaUsageType.BIOSPHERE
+        }
+        if (element is PsiSubstance) {
+            return LcaUsageType.SUBSTANCE
         }
         return null
     }
