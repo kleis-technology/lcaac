850e256838b314f40418a70239b334f360e7172b
Peva Blanchard <pblanchard@kleis.ch>
2023-03-25 20:06:41 +0100

    light refactor of quantity reference resolve

--- src/main/kotlin/ch/kleis/lcaplugin/language/psi/reference/QuantityReference.kt
+++ src/main/kotlin/ch/kleis/lcaplugin/language/psi/reference/QuantityReference.kt
@@ -2,7 +2,9 @@ package ch.kleis.lcaplugin.language.psi.reference
 
 import ch.kleis.lcaplugin.language.psi.LcaFile
 import ch.kleis.lcaplugin.language.psi.type.ref.PsiQuantityRef
+import ch.kleis.lcaplugin.psi.LcaTypes
 import com.intellij.psi.PsiElement
+import com.intellij.psi.PsiFile
 import com.intellij.psi.PsiReferenceBase
 import com.intellij.psi.ResolveState
 import com.intellij.psi.util.PsiTreeUtil
@@ -12,10 +14,15 @@ class QuantityReference(
     element: PsiQuantityRef
 ) : PsiReferenceBase<PsiQuantityRef>(element) {
     override fun resolve(): PsiElement? {
+        return tryLocally()
+            ?: tryGlobally()
+    }
+
+    private fun tryLocally(): PsiElement? {
         val resolver = QuantityRefScopeProcessor(element)
         var lastParent : PsiElement? = null
         val parents = PsiTreeUtil.collectParents(element, PsiElement::class.java, false) {
-            it.elementType == LcaFile::elementType
+            it is LcaFile
         }
         for (parent in parents) {
             val doBreak = !parent.processDeclarations(resolver, ResolveState.initial(), lastParent, element)
@@ -25,5 +32,9 @@ class QuantityReference(
             lastParent = parent
         }
         return resolver.getResult()
+    }
+
+    private fun tryGlobally(): PsiElement? {
+        return null
     }
 }
