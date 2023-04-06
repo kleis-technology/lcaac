b9f260459398729dc2359829cfb6f399f9093c19
sebdebros <sbros@kleis.ch>
2023-04-06 17:45:42 +0200

    clean code + lint

--- src/main/kotlin/ch/kleis/lcaplugin/core/allocation/Allocation.kt
+++ src/main/kotlin/ch/kleis/lcaplugin/core/allocation/Allocation.kt
@@ -5,24 +5,20 @@ import ch.kleis.lcaplugin.core.lang.value.*
 
 class Allocation {
     fun apply(system: SystemValue): SystemValue {
-        var allocatedSystem = SystemValue.empty()
-        system.processes.forEach { processValue ->
-            run {
-                val totalAllocation = totalAmount(processValue)
-                processValue.products.forEach { technoExchangeValue ->
-                    run {
-                        val allocatedProcess = ProcessValue(
-                            processValue.name,
-                            listOf(technoExchangeValue),
-                            applyAllocationToInputs(processValue.inputs, technoExchangeValue.allocation, totalAllocation),
-                            applyAllocationToBioSphere(processValue.biosphere, technoExchangeValue.allocation, totalAllocation)
-                        )
-                        allocatedSystem = allocatedSystem.plus(allocatedProcess)
-                    }
-                }
-            }
-        }
-        return SystemValue(allocatedSystem.processes, system.substanceCharacterizations)
+        val processes = system.processes.flatMap { processValue ->
+            processValue.products.map { allocateProduct(it, processValue) }
+        }.toSet()
+        return SystemValue(processes, system.substanceCharacterizations)
+    }
+
+    private fun allocateProduct(technoExchangeValue: TechnoExchangeValue, processValue: ProcessValue): ProcessValue {
+        val totalAllocation = totalAmount(processValue)
+        return ProcessValue(
+            processValue.name,
+            listOf(technoExchangeValue),
+            applyAllocationToInputs(processValue.inputs, technoExchangeValue.allocation, totalAllocation),
+            applyAllocationToBioSphere(processValue.biosphere, technoExchangeValue.allocation, totalAllocation)
+        )
     }
 
     fun totalAmount(processValue: ProcessValue): Double {
@@ -31,19 +27,27 @@ class Allocation {
     }
 
     fun allocationUnitCheck(processValue: ProcessValue) {
-        if (processValue.products.map { it.allocation.unit.dimension }.distinct().count() > 1){
+        if (processValue.products.map { it.allocation.unit.dimension }.distinct().count() > 1) {
             throw EvaluatorException("non-consistent allocation units for process ${processValue.name}")
         }
     }
 
-    private fun applyAllocationToInputs(inputs: List<TechnoExchangeValue>, allocation: QuantityValue, totalAllocation: Double): List<TechnoExchangeValue>{
+    private fun applyAllocationToInputs(
+        inputs: List<TechnoExchangeValue>,
+        allocation: QuantityValue,
+        totalAllocation: Double
+    ): List<TechnoExchangeValue> {
         return inputs.map { applyAllocationToInput(it, allocation, totalAllocation) }
     }
 
-    private fun applyAllocationToInput(technoExchangeValue: TechnoExchangeValue, allocation: QuantityValue, totalAllocation: Double): TechnoExchangeValue {
+    private fun applyAllocationToInput(
+        technoExchangeValue: TechnoExchangeValue,
+        allocation: QuantityValue,
+        totalAllocation: Double
+    ): TechnoExchangeValue {
         return TechnoExchangeValue(
             QuantityValue(
-                technoExchangeValue.quantity.amount*allocation.referenceValue()/totalAllocation,
+                technoExchangeValue.quantity.amount * allocation.referenceValue() / totalAllocation,
                 technoExchangeValue.quantity.unit
             ),
             technoExchangeValue.product,
@@ -51,14 +55,22 @@ class Allocation {
         )
     }
 
-    private fun applyAllocationToBioSphere(biosphere: List<BioExchangeValue>, allocation: QuantityValue, totalAllocation: Double): List<BioExchangeValue>{
+    private fun applyAllocationToBioSphere(
+        biosphere: List<BioExchangeValue>,
+        allocation: QuantityValue,
+        totalAllocation: Double
+    ): List<BioExchangeValue> {
         return biosphere.map { applyAllocationToBioExchange(it, allocation, totalAllocation) }
     }
 
-    private fun applyAllocationToBioExchange(bioExchange: BioExchangeValue, allocation: QuantityValue, totalAllocation: Double): BioExchangeValue {
+    private fun applyAllocationToBioExchange(
+        bioExchange: BioExchangeValue,
+        allocation: QuantityValue,
+        totalAllocation: Double
+    ): BioExchangeValue {
         return BioExchangeValue(
             QuantityValue(
-                bioExchange.quantity.amount*allocation.referenceValue()/totalAllocation,
+                bioExchange.quantity.amount * allocation.referenceValue() / totalAllocation,
                 bioExchange.quantity.unit
             ),
             bioExchange.substance
