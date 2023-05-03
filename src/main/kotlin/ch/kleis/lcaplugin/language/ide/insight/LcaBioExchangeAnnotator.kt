957c7ab337ce6b1d83a63580121de8b013270530
Peva Blanchard <pblanchard@kleis.ch>
2023-05-03 12:03:37 +0200

    bio exchange annotator : enhance message

--- src/main/kotlin/ch/kleis/lcaplugin/language/ide/insight/LcaBioExchangeAnnotator.kt
+++ src/main/kotlin/ch/kleis/lcaplugin/language/ide/insight/LcaBioExchangeAnnotator.kt
@@ -2,6 +2,7 @@ package ch.kleis.lcaplugin.language.ide.insight
 
 import ch.kleis.lcaplugin.language.psi.type.PsiSubstance
 import ch.kleis.lcaplugin.language.psi.type.exchange.PsiBioExchange
+import ch.kleis.lcaplugin.language.psi.type.spec.PsiSubstanceSpec
 import com.intellij.codeInspection.ProblemHighlightType
 import com.intellij.lang.annotation.AnnotationHolder
 import com.intellij.lang.annotation.Annotator
@@ -16,11 +17,21 @@ class LcaBioExchangeAnnotator : Annotator {
 
         val target = element.getSubstanceSpec().reference.resolve()
         if (target == null || target !is PsiSubstance) {
-            val name = element.getSubstanceSpec().getSubstanceRef().name
-            holder.newAnnotation(HighlightSeverity.WARNING, "unresolved substance $name")
-                .range(element.getSubstanceSpec())
+            val spec = element.getSubstanceSpec()
+            holder.newAnnotation(HighlightSeverity.WARNING, "unresolved substance ${render(spec)}")
+                .range(spec)
                 .highlightType(ProblemHighlightType.WARNING)
                 .create()
         }
+    }
+
+    private fun render(spec: PsiSubstanceSpec): String {
+        val compartmentField = spec.getCompartmentField()?.getValue()?.let { """compartment="$it"""" }
+        val subCompartmentField = spec.getSubCompartmentField()?.getValue()?.let { """sub_compartment="$it"""" }
+        val args = listOfNotNull(
+            compartmentField,
+            subCompartmentField,
+        ).joinToString()
+        return if (args.isBlank()) spec.name else "${spec.name}(${args})"
     }
 }
--- src/test/kotlin/ch/kleis/lcaplugin/language/ide/insight/LcaBioExchangeAnnotatorTest.kt
+++ src/test/kotlin/ch/kleis/lcaplugin/language/ide/insight/LcaBioExchangeAnnotatorTest.kt
@@ -23,7 +23,7 @@ class LcaBioExchangeAnnotatorTest : BasePlatformTestCase() {
             
             process p {
                 emissions {
-                    1 kg co2
+                    1 kg co2(compartment = "air")
                 }
             }
         """.trimIndent()
@@ -37,7 +37,12 @@ class LcaBioExchangeAnnotatorTest : BasePlatformTestCase() {
         annotator.annotate(element, mock.holder)
 
         // then
-        verify { mock.holder.newAnnotation(HighlightSeverity.WARNING, "unresolved substance co2") }
+        verify {
+            mock.holder.newAnnotation(
+                HighlightSeverity.WARNING,
+                """unresolved substance co2(compartment="air")"""
+            )
+        }
         verify { mock.builder.range(element.getSubstanceSpec()) }
         verify { mock.builder.highlightType(ProblemHighlightType.WARNING) }
         verify { mock.builder.create() }
