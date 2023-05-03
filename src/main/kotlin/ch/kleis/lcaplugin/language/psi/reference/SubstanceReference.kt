2d0f322c880c63ea1387ce3424f68c33342af9a2
Peva Blanchard <pblanchard@kleis.ch>
2023-05-03 10:42:41 +0200

    psi substance reference test

--- src/main/kotlin/ch/kleis/lcaplugin/language/psi/reference/SubstanceReference.kt
+++ src/main/kotlin/ch/kleis/lcaplugin/language/psi/reference/SubstanceReference.kt
@@ -3,17 +3,14 @@ package ch.kleis.lcaplugin.language.psi.reference
 import ch.kleis.lcaplugin.language.psi.LcaFile
 import ch.kleis.lcaplugin.language.psi.stub.LcaStubIndexKeys
 import ch.kleis.lcaplugin.language.psi.stub.substance.SubstanceKeyIndex
+import ch.kleis.lcaplugin.language.psi.type.PsiSubstance
 import ch.kleis.lcaplugin.language.psi.type.spec.PsiSubstanceSpec
 import com.intellij.codeInsight.lookup.LookupElementBuilder
-import com.intellij.psi.PsiElement
-import com.intellij.psi.PsiElementResolveResult
-import com.intellij.psi.PsiPolyVariantReference
-import com.intellij.psi.PsiReferenceBase
-import com.intellij.psi.ResolveResult
+import com.intellij.psi.*
 import com.intellij.psi.stubs.StubIndex
 
 class SubstanceReference(
-    element: PsiSubstanceSpec
+        element: PsiSubstanceSpec
 ) : PsiReferenceBase<PsiSubstanceSpec>(element), PsiPolyVariantReference {
     private val project = element.project
     private val file = element.containingFile as LcaFile
@@ -29,36 +26,45 @@ class SubstanceReference(
     override fun multiResolve(incompleteCode: Boolean): Array<ResolveResult> {
         val type = element.getType()?.value ?: return emptyArray()
         val compartment = element.getCompartmentField()?.getValue() ?: return emptyArray()
-        val subCompartment = element.getSubCompartmentField()?.getValue()
+
         val candidateFqns = allPkgNames.map {
             "$it.${element.getSubstanceRef().name}"
         }
-        return candidateFqns
-            .flatMap { fqn ->
-                SubstanceKeyIndex.findSubstances(
+
+        val subCompartment = element.getSubCompartmentField()?.getValue()
+
+        fun findSubstances(subCompartment: String?): (String) -> Collection<PsiSubstance> = { fqn: String ->
+            SubstanceKeyIndex.findSubstances(
                     project,
                     fqn, type, compartment, subCompartment,
-                )
-            }
-            .map { PsiElementResolveResult(it) }
-            .toTypedArray()
+            )
+        }
+
+        return if (subCompartment != null) {
+            candidateFqns
+                    .flatMap(findSubstances(subCompartment))
+                    .ifEmpty { candidateFqns.flatMap(findSubstances(null)) }
+        } else {
+            candidateFqns.flatMap(findSubstances(null))
+        }.map(::PsiElementResolveResult).toTypedArray()
+
     }
 
     override fun getVariants(): Array<Any> {
         val allKeys = StubIndex.getInstance().getAllKeys(LcaStubIndexKeys.SUBSTANCES, project)
         val filter = allKeys
-            .filter { key ->
-                allPkgNames.any {
-                    val parts = key.split(".")
-                    val prefix = parts.take(parts.size - 1).joinToString(".")
-                    prefix.startsWith(it)
+                .filter { key ->
+                    allPkgNames.any {
+                        val parts = key.split(".")
+                        val prefix = parts.take(parts.size - 1).joinToString(".")
+                        prefix.startsWith(it)
+                    }
                 }
-            }
         val map = filter
-            .map { it.split(".").last() }
+                .map { it.split(".").last() }
         val map1 = map
-            .map { LookupElementBuilder.create(it) }
+                .map { LookupElementBuilder.create(it) }
         return map1
-            .toTypedArray()
+                .toTypedArray()
     }
 }
--- src/test/kotlin/ch/kleis/lcaplugin/language/psi/reference/SubstanceReferenceTest.kt
+++ src/test/kotlin/ch/kleis/lcaplugin/language/psi/reference/SubstanceReferenceTest.kt
@@ -10,31 +10,170 @@ import org.junit.Test
 class SubstanceReferenceTest : BasePlatformTestCase() {
 
     override fun getTestDataPath(): String {
-        return "testdata/language/psi/reference/subst"
-    }
-
-    override fun setUp() {
-        super.setUp()
-        myFixture.copyDirectoryToProject("", "")
+        return "testdata"
     }
 
     @Test
-    fun test_resolve() {
+    fun test_resolve_whenNoSubCompartment_shouldDefaultToMatchingCompartment() {
         // given
-        val pkgName = "language.psi.reference.subst.test_resolve"
+        val pkgName = "language.psi.reference.subst.test_resolve_whenNoSubCompartment_shouldDefaultToMatchingCompartment"
+        myFixture.createFile(
+                "$pkgName.co2_air.lca", """
+                package $pkgName.co2_air
+               
+                substance co2 {
+                    name = "co2"
+                    type = Emission
+                    compartment = "air"
+                    reference_unit = kg
+                }
+
+                substance co2 {
+                    name = "co2"
+                    type = Emission
+                    compartment = "air"
+                    sub_compartment = "another"
+                    reference_unit = kg
+                }
+            """.trimIndent()
+        )
+        myFixture.createFile(
+                "$pkgName.lca", """
+                package $pkgName
+
+                import $pkgName.co2_air
+
+                process p {
+                    products {
+                        1 kg a
+                    }
+                    emissions {
+                        1 kg co2(compartment="air", sub_compartment="nothing")
+                    }
+                }
+            """.trimIndent()
+        )
         val substanceSpec = ProcessStubKeyIndex.findProcesses(project, "$pkgName.p").first()
-            .getEmissions().first()
-            .getSubstanceSpec()
+                .getEmissions().first()
+                .getSubstanceSpec()
 
         // when
         val actual = substanceSpec.reference.resolve()
 
         // then
         val expected = SubstanceKeyIndex.findSubstances(
-            project,
-            "$pkgName.co2_air.co2_air",
-            "Emission",
-            "air"
+                project,
+                "$pkgName.co2_air.co2",
+                "Emission",
+                "air"
+        ).first()
+        TestCase.assertEquals(expected, actual)
+    }
+
+    @Test
+    fun test_resolve_whenIncompatibleTypes_ShouldNotResolve() {
+        // given
+        val pkgName = "language.psi.reference.subst.test_resolve_whenIncompatibleTypes_ShouldNotResolve"
+        myFixture.createFile(
+                "$pkgName.co2_air.lca", """
+                package $pkgName.co2_air
+               
+                substance co2 {
+                    name = "co2"
+                    type = Emission
+                    compartment = "air"
+                    reference_unit = kg
+                }
+
+                substance co2 {
+                    name = "co2"
+                    type = Emission
+                    compartment = "air"
+                    sub_compartment = "another"
+                    reference_unit = kg
+                }
+            """.trimIndent()
+        )
+        myFixture.createFile(
+                "$pkgName.lca", """
+                package $pkgName
+
+                import $pkgName.co2_air
+
+                process p {
+                    products {
+                        1 kg a
+                    }
+                    resources {
+                        1 kg co2(compartment="air", sub_compartment="nothing")
+                    }
+                }
+            """.trimIndent()
+        )
+        val substanceSpec = ProcessStubKeyIndex.findProcesses(project, "$pkgName.p").first()
+                .getResources().first()
+                .getSubstanceSpec()
+
+        // when
+        val actual = substanceSpec.reference.resolve()
+
+        // then
+        assertNull(actual)
+    }
+
+    @Test
+    fun test_resolve__whenExact() {
+        // given
+        val pkgName = "language.psi.reference.subst.test_resolve__whenExact"
+        myFixture.createFile(
+                "$pkgName.co2_air.lca", """
+                package $pkgName.co2_air
+               
+                substance co2 {
+                    name = "co2"
+                    type = Emission
+                    compartment = "air"
+                    reference_unit = kg
+                }
+
+                substance co2 {
+                    name = "co2"
+                    type = Emission
+                    compartment = "air"
+                    sub_compartment = "another"
+                    reference_unit = kg
+                }
+            """.trimIndent()
+        )
+        myFixture.createFile(
+                "$pkgName.lca", """
+                package $pkgName
+
+                import $pkgName.co2_air
+
+                process p {
+                    products {
+                        1 kg a
+                    }
+                    emissions {
+                        1 kg co2(compartment="air")
+                    }
+                }
+            """.trimIndent()
+        )
+        val substanceSpec = ProcessStubKeyIndex.findProcesses(project, "$pkgName.p").first()
+                .getEmissions().first()
+                .getSubstanceSpec()
+
+        // when
+        val actual = substanceSpec.reference.resolve()
+
+        // then
+        val expected = SubstanceKeyIndex.findSubstances(
+                project,
+                "$pkgName.co2_air.co2",
+                "Emission",
+                "air"
         ).first()
         TestCase.assertEquals(expected, actual)
     }
@@ -42,20 +181,56 @@ class SubstanceReferenceTest : BasePlatformTestCase() {
     @Test
     fun test_getVariants() {
         // given
-        val pkgName = "language.psi.reference.subst.test_resolve"
+        val pkgName = "language.psi.reference.subst.test_getVariants"
+        myFixture.createFile(
+                "$pkgName.co2_air.lca", """
+                package $pkgName.co2_air
+               
+                substance co2_air {
+                    name = "co2"
+                    type = Emission
+                    compartment = "air"
+                    reference_unit = kg
+                }
+
+                substance another_co2_air {
+                    name = "co2"
+                    type = Emission
+                    compartment = "air"
+                    sub_compartment = "another"
+                    reference_unit = kg
+                }
+            """.trimIndent()
+        )
+        myFixture.createFile(
+                "$pkgName.lca", """
+                package $pkgName
+
+                import $pkgName.co2_air
+
+                process p {
+                    products {
+                        1 kg a
+                    }
+                    emissions {
+                        1 kg co2_air(compartment="air")
+                    }
+                }
+            """.trimIndent()
+        )
         val spec = ProcessStubKeyIndex.findProcesses(project, "$pkgName.p").first()
-            .getEmissions().first()
-            .getSubstanceSpec()
+                .getEmissions().first()
+                .getSubstanceSpec()
 
         // when
         val actual = spec.reference
-            .variants.map { (it as LookupElementBuilder).lookupString }
-            .sorted()
+                .variants.map { (it as LookupElementBuilder).lookupString }
+                .sorted()
 
         // then
         val expected = listOf(
-            """co2_air(type="Emission", compartment="air")""",
-            """another_co2_air(type="Emission", compartment="air", sub_compartment="another")""",
+                """co2_air(type="Emission", compartment="air")""",
+                """another_co2_air(type="Emission", compartment="air", sub_compartment="another")""",
         ).sorted()
         TestCase.assertEquals(expected, actual)
     }
