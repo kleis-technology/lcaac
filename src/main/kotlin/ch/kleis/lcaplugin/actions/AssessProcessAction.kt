f74823b05df37b4bd2c3396df15ad784f6cfbcd6
Peva Blanchard <pblanchard@kleis.ch>
2023-05-16 16:53:07 +0200

    notify user on success
    run in background, notify on success and error
    
    notification on error

--- src/main/kotlin/ch/kleis/lcaplugin/actions/AssessProcessAction.kt
+++ src/main/kotlin/ch/kleis/lcaplugin/actions/AssessProcessAction.kt
@@ -3,19 +3,21 @@ package ch.kleis.lcaplugin.actions
 import ch.kleis.lcaplugin.core.assessment.Assessment
 import ch.kleis.lcaplugin.core.lang.evaluator.Evaluator
 import ch.kleis.lcaplugin.core.lang.evaluator.EvaluatorException
-import ch.kleis.lcaplugin.core.matrix.InventoryError
 import ch.kleis.lcaplugin.language.parser.LcaFileCollector
 import ch.kleis.lcaplugin.language.parser.LcaLangAbstractParser
 import ch.kleis.lcaplugin.language.psi.LcaFile
 import com.intellij.icons.AllIcons
+import com.intellij.notification.NotificationGroupManager
+import com.intellij.notification.NotificationType
 import com.intellij.openapi.actionSystem.AnAction
 import com.intellij.openapi.actionSystem.AnActionEvent
 import com.intellij.openapi.actionSystem.LangDataKeys
+import com.intellij.openapi.application.runReadAction
 import com.intellij.openapi.diagnostic.Logger
 
 class AssessProcessAction(private val processName: String) : AnAction(
-    "Run with default parameters",
-    "Run with default parameters",
+    "Run",
+    "Run",
     AllIcons.Actions.Execute,
 ) {
     companion object {
@@ -23,26 +25,33 @@ class AssessProcessAction(private val processName: String) : AnAction(
     }
 
     override fun actionPerformed(e: AnActionEvent) {
-
         val project = e.project ?: return
         val file = e.getData(LangDataKeys.PSI_FILE) as LcaFile? ?: return
-        val collector = LcaFileCollector()
-        val parser = LcaLangAbstractParser(collector.collect(file))
 
         try {
-            val symbolTable = parser.load()
+            val symbolTable = runReadAction {
+                val collector = LcaFileCollector()
+                val parser = LcaLangAbstractParser(collector.collect(file))
+                parser.load()
+            }
             val entryPoint = symbolTable.getTemplate(processName)!!
             val system = Evaluator(symbolTable).eval(entryPoint)
             val assessment = Assessment(system)
             val result = assessment.inventory()
             DisplayInventoryResult(project, result).show()
         } catch (e: EvaluatorException) {
-            val result = InventoryError(e.message ?: "evaluator: unknown error")
-            DisplayInventoryResult(project, result).show()
+            val title = "Error while assessing $processName"
+            NotificationGroupManager.getInstance()
+                .getNotificationGroup("LcaAsCode")
+                .createNotification(title, e.message ?: "unknown error", NotificationType.ERROR)
+                .notify(project)
             LOG.warn("Unable to process computation", e)
         } catch (e: NoSuchElementException) {
-            val result = InventoryError(e.message ?: "evaluator: unknown error")
-            DisplayInventoryResult(project, result).show()
+            val title = "Error while assessing $processName"
+            NotificationGroupManager.getInstance()
+                .getNotificationGroup("LcaAsCode")
+                .createNotification(title, e.message ?: "unknown error", NotificationType.ERROR)
+                .notify(project)
             LOG.warn("Unable to process computation", e)
         }
     }
--- src/main/kotlin/ch/kleis/lcaplugin/actions/AssessProcessWithDataAction.kt
+++ src/main/kotlin/ch/kleis/lcaplugin/actions/AssessProcessWithDataAction.kt
@@ -4,15 +4,20 @@ import ch.kleis.lcaplugin.actions.csv.CsvProcessor
 import ch.kleis.lcaplugin.actions.csv.CsvRequestReader
 import ch.kleis.lcaplugin.actions.csv.CsvResultWriter
 import ch.kleis.lcaplugin.core.lang.evaluator.EvaluatorException
-import ch.kleis.lcaplugin.core.matrix.InventoryError
 import ch.kleis.lcaplugin.language.parser.LcaFileCollector
 import ch.kleis.lcaplugin.language.parser.LcaLangAbstractParser
 import ch.kleis.lcaplugin.language.psi.LcaFile
 import com.intellij.icons.AllIcons
+import com.intellij.notification.NotificationGroupManager
+import com.intellij.notification.NotificationType
 import com.intellij.openapi.actionSystem.AnAction
 import com.intellij.openapi.actionSystem.AnActionEvent
 import com.intellij.openapi.actionSystem.LangDataKeys
+import com.intellij.openapi.application.runReadAction
 import com.intellij.openapi.diagnostic.Logger
+import com.intellij.openapi.progress.ProgressIndicator
+import com.intellij.openapi.progress.ProgressManager
+import com.intellij.openapi.progress.Task
 import kotlin.io.path.Path
 
 class AssessProcessWithDataAction(
@@ -31,38 +36,63 @@ class AssessProcessWithDataAction(
         val file = e.getData(LangDataKeys.PSI_FILE) as LcaFile? ?: return
         val containingDirectory = file.containingDirectory ?: return
 
+        ProgressManager.getInstance().run(object : Task.Backgroundable(project, "Run with ${processName}.csv") {
+            override fun run(indicator: ProgressIndicator) {
+                try {
+                    // read
+                    indicator.fraction = 0.0
+                    indicator.text = "Reading $processName.csv"
+                    val csvFile = Path(containingDirectory.virtualFile.path, "$processName.csv").toFile()
+                    val requestReader = CsvRequestReader(processName, csvFile.inputStream())
+                    val requests = requestReader.read()
 
-        // TODO: stream "read, process, write", progress indicator bar, run in background
-        try {
-            // read
-            val csvFile = Path(containingDirectory.virtualFile.path, "$processName.csv").toFile()
-            val requestReader = CsvRequestReader(processName, csvFile.inputStream())
-            val requests = requestReader.read()
+                    // process
+                    val symbolTable = runReadAction {
+                        val collector = LcaFileCollector()
+                        val parser = LcaLangAbstractParser(collector.collect(file))
+                        parser.load()
+                    }
+                    val csvProcessor = CsvProcessor(symbolTable)
+                    val results = requests.map { request ->
+                        ProgressManager.checkCanceled()
+                        indicator.text = "Processing using ${request.arguments()}"
+                        indicator.fraction = indicator.fraction + 1.0 / requests.size
+                        csvProcessor.process(request)
+                    }
 
-            // process
-            val collector = LcaFileCollector()
-            val parser = LcaLangAbstractParser(collector.collect(file))
-            val symbolTable = parser.load()
-            val csvProcessor = CsvProcessor(symbolTable)
-            val results = requests.map { request ->
-                csvProcessor.process(request)
+                    // write
+                    indicator.text = "Writing to $processName.results.csv"
+                    indicator.fraction = 1.0
+                    val csvResultFile = Path(containingDirectory.virtualFile.path, "$processName.results.csv").toFile()
+                    val resultWriter = CsvResultWriter(csvResultFile.outputStream())
+                    resultWriter.write(results)
+                    resultWriter.flush()
+
+                    // done
+                    indicator.text = "Written to $processName.results.csv"
+                    indicator.fraction = 1.0
+                    val title = "${requests.size} successful assessments of process $processName"
+                    val message = "Results stored in ${processName}.results.csv"
+                    NotificationGroupManager.getInstance()
+                        .getNotificationGroup("LcaAsCode")
+                        .createNotification(title, message, NotificationType.INFORMATION)
+                        .notify(project)
+                } catch (e: EvaluatorException) {
+                    val title = "Error while assessing $processName"
+                    NotificationGroupManager.getInstance()
+                        .getNotificationGroup("LcaAsCode")
+                        .createNotification(title, e.message ?: "unknown error", NotificationType.ERROR)
+                        .notify(project)
+                    LOG.warn("Unable to process computation", e)
+                } catch (e: NoSuchElementException) {
+                    val title = "Error while assessing $processName"
+                    NotificationGroupManager.getInstance()
+                        .getNotificationGroup("LcaAsCode")
+                        .createNotification(title, e.message ?: "unknown error", NotificationType.ERROR)
+                        .notify(project)
+                    LOG.warn("Unable to process computation", e)
+                }
             }
-
-            // write
-            val csvResultFile = Path(containingDirectory.virtualFile.path, "$processName.results.csv").toFile()
-            val resultWriter = CsvResultWriter(csvResultFile.outputStream())
-            resultWriter.write(results)
-            resultWriter.flush()
-
-            // TODO: Notify user on success
-        } catch (e: EvaluatorException) {
-            val result = InventoryError(e.message ?: "evaluator: unknown error")
-            DisplayInventoryResult(project, result).show()
-            LOG.warn("Unable to process computation", e)
-        } catch (e: NoSuchElementException) {
-            val result = InventoryError(e.message ?: "evaluator: unknown error")
-            DisplayInventoryResult(project, result).show()
-            LOG.warn("Unable to process computation", e)
-        }
+        })
     }
 }
