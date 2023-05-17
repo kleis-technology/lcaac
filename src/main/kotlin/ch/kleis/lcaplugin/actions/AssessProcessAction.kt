7f551171614c45fb220839a80096c4f37b7c0531
Peva Blanchard <pblanchard@kleis.ch>
2023-05-17 11:06:47 +0200

    assess process action in background

--- src/main/kotlin/ch/kleis/lcaplugin/actions/AssessProcessAction.kt
+++ src/main/kotlin/ch/kleis/lcaplugin/actions/AssessProcessAction.kt
@@ -2,7 +2,6 @@ package ch.kleis.lcaplugin.actions
 
 import ch.kleis.lcaplugin.core.assessment.Assessment
 import ch.kleis.lcaplugin.core.lang.evaluator.Evaluator
-import ch.kleis.lcaplugin.core.lang.evaluator.EvaluatorException
 import ch.kleis.lcaplugin.core.matrix.InventoryMatrix
 import ch.kleis.lcaplugin.language.parser.LcaFileCollector
 import ch.kleis.lcaplugin.language.parser.LcaLangAbstractParser
@@ -16,6 +15,9 @@ import com.intellij.openapi.actionSystem.AnActionEvent
 import com.intellij.openapi.actionSystem.LangDataKeys
 import com.intellij.openapi.application.runReadAction
 import com.intellij.openapi.diagnostic.Logger
+import com.intellij.openapi.progress.ProgressIndicator
+import com.intellij.openapi.progress.ProgressManager
+import com.intellij.openapi.progress.Task
 import com.intellij.openapi.project.Project
 import com.intellij.openapi.wm.ToolWindowManager
 import com.intellij.ui.content.ContentFactory
@@ -32,43 +34,55 @@ class AssessProcessAction(private val processName: String) : AnAction(
     override fun actionPerformed(e: AnActionEvent) {
         val project = e.project ?: return
         val file = e.getData(LangDataKeys.PSI_FILE) as LcaFile? ?: return
+        ProgressManager.getInstance().run(object : Task.Backgroundable(project, "Run") {
+            private var inventory: InventoryMatrix? = null
 
-        try {
-            val symbolTable = runReadAction {
-                val collector = LcaFileCollector()
-                val parser = LcaLangAbstractParser(collector.collect(file))
-                parser.load()
+            override fun run(indicator: ProgressIndicator) {
+                // read
+                indicator.fraction = 0.0
+                indicator.text = "Loading symbol table"
+                val symbolTable = runReadAction {
+                    val collector = LcaFileCollector()
+                    val parser = LcaLangAbstractParser(collector.collect(file))
+                    parser.load()
+                }
+
+                // compute
+                indicator.fraction = 0.33
+                indicator.text = "Solving system"
+                val entryPoint =
+                    symbolTable.getTemplate(processName)!! // We are called from a process, so it must exist
+                val system = Evaluator(symbolTable).eval(entryPoint)
+                this.inventory = Assessment(system).inventory()
             }
-            val entryPoint = symbolTable.getTemplate(processName)!!
-            val system = Evaluator(symbolTable).eval(entryPoint)
-            val inventory = Assessment(system).inventory()
-            displayInventory(project, inventory)
-        } catch (e: EvaluatorException) {
-            val title = "Error while assessing $processName"
-            NotificationGroupManager.getInstance()
-                .getNotificationGroup("LcaAsCode")
-                .createNotification(title, e.message ?: "unknown error", NotificationType.ERROR)
-                .notify(project)
-            LOG.warn("Unable to process computation", e)
-        } catch (e: NoSuchElementException) {
-            val title = "Error while assessing $processName"
-            NotificationGroupManager.getInstance()
-                .getNotificationGroup("LcaAsCode")
-                .createNotification(title, e.message ?: "unknown error", NotificationType.ERROR)
-                .notify(project)
-            LOG.warn("Unable to process computation", e)
-        }
+
+            override fun onSuccess() {
+                this.inventory?.let {
+                    displayInventory(project, it)
+                }
+            }
+
+            override fun onThrowable(e: Throwable) {
+                val title = "Error while assessing $processName"
+                NotificationGroupManager.getInstance()
+                    .getNotificationGroup("LcaAsCode")
+                    .createNotification(title, e.message ?: "unknown error", NotificationType.ERROR)
+                    .notify(project)
+                LOG.warn("Unable to process computation", e)
+            }
+
+            private fun displayInventory(project: Project, inventory: InventoryMatrix) {
+                val toolWindow = ToolWindowManager.getInstance(project).getToolWindow("LCA Output") ?: return
+                val lcaProcessAssessResult = LcaProcessAssessResult(inventory)
+                val content =
+                    ContentFactory.getInstance().createContent(lcaProcessAssessResult.getContent(), project.name, false)
+                toolWindow.contentManager.addContent(content)
+                toolWindow.contentManager.setSelectedContent(content)
+                toolWindow.show()
+            }
+        })
     }
 
-    private fun displayInventory(project: Project, inventory: InventoryMatrix) {
-        val toolWindow = ToolWindowManager.getInstance(project).getToolWindow("LCA Output") ?: return
-        val lcaProcessAssessResult = LcaProcessAssessResult(inventory)
-        val content =
-            ContentFactory.getInstance().createContent(lcaProcessAssessResult.getContent(), project.name, false)
-        toolWindow.contentManager.addContent(content)
-        toolWindow.contentManager.setSelectedContent(content)
-        toolWindow.show()
-    }
 }
 
 
