01a37d636cd8225ff84ea8aef2448738761a77c9
Peva Blanchard <pblanchard@kleis.ch>
2023-05-17 11:01:43 +0200

    graph child processes action as a background task

--- src/main/kotlin/ch/kleis/lcaplugin/actions/GraphChildProcessesAction.kt
+++ src/main/kotlin/ch/kleis/lcaplugin/actions/GraphChildProcessesAction.kt
@@ -4,7 +4,6 @@ import ch.kleis.lcaplugin.MyBundle
 import ch.kleis.lcaplugin.core.graph.Graph
 import ch.kleis.lcaplugin.core.graph.GraphLink
 import ch.kleis.lcaplugin.core.graph.GraphNode
-import ch.kleis.lcaplugin.core.lang.SymbolTable
 import ch.kleis.lcaplugin.core.lang.evaluator.Evaluator
 import ch.kleis.lcaplugin.core.lang.value.SystemValue
 import ch.kleis.lcaplugin.language.parser.LcaFileCollector
@@ -17,7 +16,11 @@ import com.intellij.notification.NotificationType
 import com.intellij.openapi.actionSystem.AnAction
 import com.intellij.openapi.actionSystem.AnActionEvent
 import com.intellij.openapi.actionSystem.LangDataKeys
+import com.intellij.openapi.application.runReadAction
 import com.intellij.openapi.diagnostic.Logger
+import com.intellij.openapi.progress.ProgressIndicator
+import com.intellij.openapi.progress.ProgressManager
+import com.intellij.openapi.progress.Task
 import com.intellij.openapi.project.Project
 import com.intellij.openapi.project.ProjectManager
 import com.intellij.openapi.wm.ToolWindowManager
@@ -44,74 +47,101 @@ class GraphChildProcessesAction(private val processName: String) : AnAction(
     override fun actionPerformed(e: AnActionEvent) {
         val project = e.project ?: return
         val file = e.getData(LangDataKeys.PSI_FILE) as LcaFile? ?: return
-        val symbolTable = buildSymbolTable(file)
-        val entryPoint = symbolTable.getTemplate(processName)!! // We are called from a process, so it must exist
-        val systemValue = Evaluator(symbolTable).eval(entryPoint)
-        val graph = buildSystemProcessGraph(systemValue)
-        val content = try {
-            buildContent(processName, graph)
-        } catch (e: Exception) {
-            buildErrorContent(processName, e)
-        }
-        fillAndShowToolWindow(project, content)
-    }
+        ProgressManager.getInstance().run(object : Task.Backgroundable(project, "Generate graph") {
+            private var graph: Graph? = null
 
-    private fun buildSymbolTable(file: LcaFile): SymbolTable =
-        LcaLangAbstractParser(LcaFileCollector().collect(file)).load()
+            override fun run(indicator: ProgressIndicator) {
+                // read
+                indicator.fraction = 0.0
+                indicator.text = "Loading symbol table"
+                val symbolTable = runReadAction {
+                    val collector = LcaFileCollector()
+                    val parser = LcaLangAbstractParser(collector.collect(file))
+                    parser.load()
+                }
 
-    /**
-     * Format an error in Content form for consumption by the IntelliJ ToolWindow API.
-     */
-    @Suppress("DialogTitleCapitalization")
-    private fun buildErrorContent(processName: String, error: Exception): Content {
-        val title = MyBundle.message("lca.dialog.graph.error", processName)
-        val msg = error.message ?: "An unknown error has occurred."
-        NotificationGroupManager.getInstance()
-            .getNotificationGroup("LcaAsCode")
-            .createNotification(title, msg, NotificationType.WARNING)
-            .notify(ProjectManager.getInstance().openProjects.firstOrNull())
-        LOG.warn(title, error)
-        return ContentFactory.getInstance().createContent(
-            JTextArea(msg.substring(0, min(msg.length, 400))), title, false
-        )
-    }
+                // compute
+                indicator.fraction = 0.33
+                indicator.text = "Solving system"
+                val entryPoint =
+                    symbolTable.getTemplate(processName)!! // We are called from a process, so it must exist
+                val systemValue = Evaluator(symbolTable).eval(entryPoint)
 
-    /**
-     * Format the data in Content form for consumption by the IntelliJ ToolWindow API.
-     */
-    private fun buildContent(processName: String, graph: Graph): Content = ContentFactory.getInstance().createContent(
-        LcaGraphChildProcessesResult(graph).getContent(), "ProcessGraph for process $processName", false
-    )
-
-    private fun fillAndShowToolWindow(project: Project, content: Content) {
-        val toolWindow = ToolWindowManager.getInstance(project).getToolWindow("LCA Output") ?: return
-        toolWindow.contentManager.addContent(content)
-        toolWindow.contentManager.setSelectedContent(content)
-        toolWindow.show()
-    }
-
-    /* This is written under the understanding that a system built using an Evaluator and an entry point contains
-     * completely and exclusively the processes related to that entry point.
-     */
-    private fun buildSystemProcessGraph(systemValue: SystemValue): Graph {
-        return systemValue.processes.fold(Graph.empty()) { graph, processValue ->
-            val processKey = "PROCESS_${processValue.name}"
-            val processNode = GraphNode(processValue.name)
-
-            val productsGraph = processValue.products.fold(Graph.empty()) { g, xchange ->
-                g.addNode(GraphNode(xchange)).addLink(GraphLink(false, processKey, xchange))
+                // generate graph
+                indicator.fraction = 0.66
+                indicator.text = "Generating graph"
+                this.graph = buildSystemProcessGraph(systemValue)
             }
 
-            val inputsGraph = processValue.inputs.fold(Graph.empty()) { g, xchange ->
-                g.addNode(GraphNode(xchange)).addLink(GraphLink(true, processKey, xchange))
+            override fun onSuccess() {
+                graph?.let {
+                    val content = buildContent(processName, it)
+                    fillAndShowToolWindow(project, content)
+                }
             }
 
-            val biosphereGraph = processValue.biosphere.fold((Graph.empty())) { g, xchange ->
-                g.addNode(GraphNode(xchange)).addLink(GraphLink(processKey, xchange))
+            override fun onThrowable(error: Throwable) {
+                val content = buildErrorContent(processName, error)
+                fillAndShowToolWindow(project, content)
             }
 
-            graph.addNode(processNode).merge(productsGraph, inputsGraph, biosphereGraph)
-        }
+            /**
+             * Format an error in Content form for consumption by the IntelliJ ToolWindow API.
+             */
+            @Suppress("DialogTitleCapitalization")
+            private fun buildErrorContent(processName: String, error: Throwable): Content {
+                val title = MyBundle.message("lca.dialog.graph.error", processName)
+                val msg = error.message ?: "An unknown error has occurred."
+                NotificationGroupManager.getInstance()
+                    .getNotificationGroup("LcaAsCode")
+                    .createNotification(title, msg, NotificationType.WARNING)
+                    .notify(ProjectManager.getInstance().openProjects.firstOrNull())
+                LOG.warn(title, error)
+                return ContentFactory.getInstance().createContent(
+                    JTextArea(msg.substring(0, min(msg.length, 400))), title, false
+                )
+            }
+
+            /**
+             * Format the data in Content form for consumption by the IntelliJ ToolWindow API.
+             */
+            private fun buildContent(processName: String, graph: Graph): Content =
+                ContentFactory.getInstance().createContent(
+                    LcaGraphChildProcessesResult(graph).getContent(), "ProcessGraph for process $processName", false
+                )
+
+            private fun fillAndShowToolWindow(project: Project, content: Content) {
+                val toolWindow = ToolWindowManager.getInstance(project).getToolWindow("LCA Output") ?: return
+                toolWindow.contentManager.addContent(content)
+                toolWindow.contentManager.setSelectedContent(content)
+                toolWindow.show()
+            }
+
+            /* This is written under the understanding that a system built using an Evaluator and an entry point contains
+             * completely and exclusively the processes related to that entry point.
+             */
+            private fun buildSystemProcessGraph(systemValue: SystemValue): Graph {
+                return systemValue.processes.fold(Graph.empty()) { graph, processValue ->
+                    val processKey = "PROCESS_${processValue.name}"
+                    val processNode = GraphNode(processValue.name)
+
+                    val productsGraph = processValue.products.fold(Graph.empty()) { g, xchange ->
+                        g.addNode(GraphNode(xchange)).addLink(GraphLink(false, processKey, xchange))
+                    }
+
+                    val inputsGraph = processValue.inputs.fold(Graph.empty()) { g, xchange ->
+                        g.addNode(GraphNode(xchange)).addLink(GraphLink(true, processKey, xchange))
+                    }
+
+                    val biosphereGraph = processValue.biosphere.fold((Graph.empty())) { g, xchange ->
+                        g.addNode(GraphNode(xchange)).addLink(GraphLink(processKey, xchange))
+                    }
+
+                    graph.addNode(processNode).merge(productsGraph, inputsGraph, biosphereGraph)
+                }
+            }
+        })
     }
+
 
 }
