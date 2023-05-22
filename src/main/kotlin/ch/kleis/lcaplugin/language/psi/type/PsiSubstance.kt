ff08929f2a7806365abe7d9913f2cafd9473ff10
Joachim Desroches <jdesroches@kleis.ch>
2023-05-22 11:21:59 +0200

    Remove unused mixin and interface for impact block.

--- src/main/kotlin/ch/kleis/lcaplugin/language/psi/type/PsiSubstance.kt
+++ src/main/kotlin/ch/kleis/lcaplugin/language/psi/type/PsiSubstance.kt
@@ -1,13 +1,13 @@
 package ch.kleis.lcaplugin.language.psi.type
 
 import ch.kleis.lcaplugin.language.psi.stub.substance.SubstanceStub
-import ch.kleis.lcaplugin.language.psi.type.block.PsiBlockImpacts
-import ch.kleis.lcaplugin.language.psi.type.exchange.PsiImpactExchange
 import ch.kleis.lcaplugin.language.psi.type.field.PsiStringLiteralField
 import ch.kleis.lcaplugin.language.psi.type.field.PsiSubstanceTypeField
 import ch.kleis.lcaplugin.language.psi.type.field.PsiUnitField
 import ch.kleis.lcaplugin.language.psi.type.ref.PsiSubstanceRef
 import ch.kleis.lcaplugin.language.psi.type.trait.BlockMetaOwner
+import ch.kleis.lcaplugin.psi.LcaBlockImpacts
+import ch.kleis.lcaplugin.psi.LcaImpactExchange
 import ch.kleis.lcaplugin.psi.LcaTypes
 import com.intellij.psi.PsiElement
 import com.intellij.psi.PsiNameIdentifierOwner
@@ -61,17 +61,13 @@ interface PsiSubstance : BlockMetaOwner, PsiNameIdentifierOwner, StubBasedPsiEle
         return node.findChildByType(LcaTypes.REFERENCE_UNIT_FIELD)?.psi as PsiUnitField
     }
 
-    fun hasImpacts(): Boolean {
-        return node.findChildByType(LcaTypes.BLOCK_IMPACTS) != null
-    }
-
-    fun getBlockImpacts(): Collection<PsiBlockImpacts> {
+    fun getBlockImpacts(): Collection<LcaBlockImpacts> {
         return node.getChildren(TokenSet.create(LcaTypes.BLOCK_IMPACTS))
-            .map { it.psi as PsiBlockImpacts }
+            .map { it.psi as LcaBlockImpacts }
     }
 
-    fun getImpactExchanges(): Collection<PsiImpactExchange> {
+    fun getImpactExchanges(): List<LcaImpactExchange> {
         return getBlockImpacts()
-            .flatMap { it.getExchanges() }
+            .flatMap { it.impactExchangeList }
     }
 }
