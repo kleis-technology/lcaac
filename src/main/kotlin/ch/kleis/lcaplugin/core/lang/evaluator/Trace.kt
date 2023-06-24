e1090a514159e64c17cef4a712afbd3ecbac4f2e
Peva Blanchard <pblanchard@kleis.ch>
2023-06-24 18:26:45 +0200

    trace: get product (bfs) order

--- src/main/kotlin/ch/kleis/lcaplugin/core/lang/evaluator/Trace.kt
+++ src/main/kotlin/ch/kleis/lcaplugin/core/lang/evaluator/Trace.kt
@@ -1,19 +1,33 @@
 package ch.kleis.lcaplugin.core.lang.evaluator
 
-import ch.kleis.lcaplugin.core.lang.value.MatrixRowIndex
-import ch.kleis.lcaplugin.core.lang.value.ProcessValue
-import ch.kleis.lcaplugin.core.lang.value.SubstanceCharacterizationValue
-import ch.kleis.lcaplugin.core.lang.value.SystemValue
+import ch.kleis.lcaplugin.core.lang.value.*
 
 class Trace {
     private val stages = ArrayList<HashSet<MatrixRowIndex>>()
     private var currentStage = HashSet<MatrixRowIndex>()
     private val processes = HashSet<ProcessValue>()
     private val substanceCharacterizations = HashSet<SubstanceCharacterizationValue>()
+    private val productDepthMap = HashMap<MatrixColumnIndex, Int>()
 
     companion object {
         fun empty(): Trace {
             return Trace()
+        }
+    }
+
+    fun getProductOrder(): Comparator<MatrixColumnIndex> {
+        return object : Comparator<MatrixColumnIndex> {
+            override fun compare(o1: MatrixColumnIndex, o2: MatrixColumnIndex): Int {
+                val d1 = productDepthMap[o1] ?: throw EvaluatorException("unknown ${o1}")
+                val d2 = productDepthMap[o2] ?: throw EvaluatorException("unknown ${o2}")
+                if (d1 < d2) {
+                    return -1
+                }
+                if (d1 > d2) {
+                    return 1
+                }
+                return o1.getUID().compareTo(o2.getUID())
+            }
         }
     }
 
@@ -55,9 +69,25 @@ class Trace {
     }
 
     fun commit() {
-        if (currentStage.isNotEmpty()) {
-            stages.add(currentStage)
-            currentStage = HashSet()
+        if (currentStage.isEmpty()) {
+            return
         }
+
+        val currentDepth = stages.size
+        currentStage
+            .filterIsInstance<ProcessValue>()
+            .forEach { process ->
+                process.products.forEach { exchange ->
+                    productDepthMap[exchange.product] = currentDepth
+                }
+            }
+        currentStage
+            .filterIsInstance<SubstanceCharacterizationValue>()
+            .forEach { sc ->
+                productDepthMap[sc.referenceExchange.substance] = currentDepth
+            }
+
+        stages.add(currentStage)
+        currentStage = HashSet()
     }
 }
