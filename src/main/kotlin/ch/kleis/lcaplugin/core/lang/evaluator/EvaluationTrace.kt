0a7a06694ddfca999715f6f2cdf80210b525cc99
Peva Blanchard <pblanchard@kleis.ch>
2023-09-01 00:53:13 +0200

    removed todo

--- src/main/kotlin/ch/kleis/lcaplugin/core/lang/evaluator/EvaluationTrace.kt
+++ src/main/kotlin/ch/kleis/lcaplugin/core/lang/evaluator/EvaluationTrace.kt
@@ -10,7 +10,6 @@ class EvaluationTrace<Q> {
     private val observableDepthMap = HashMap<MatrixColumnIndex<Q>, Int>()
 
     companion object {
-        // TODO: Arguments here
         fun <Q> empty(): EvaluationTrace<Q> {
             return EvaluationTrace()
         }
