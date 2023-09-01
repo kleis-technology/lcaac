d0098881ee7c8f41d6ba3c701cc713a9fc3085aa
Peva Blanchard <pblanchard@kleis.ch>
2023-09-01 13:38:51 +0200

    sensitivity analysis: guard agains missing parameters and 1000+ processes

--- src/main/kotlin/ch/kleis/lcaplugin/core/assessment/SensitivityAnalysisProgram.kt
+++ src/main/kotlin/ch/kleis/lcaplugin/core/assessment/SensitivityAnalysisProgram.kt
@@ -1,5 +1,6 @@
 package ch.kleis.lcaplugin.core.assessment
 
+import ch.kleis.lcaplugin.core.lang.evaluator.EvaluatorException
 import ch.kleis.lcaplugin.core.lang.value.ProcessValue
 import ch.kleis.lcaplugin.core.lang.value.SystemValue
 import ch.kleis.lcaplugin.core.math.dual.DualNumber
@@ -12,6 +13,12 @@ class SensitivityAnalysisProgram(
     private val parameters: ParameterVector<DualNumber>,
 ) {
     fun run(): SensitivityAnalysis {
+        if (parameters.names.size() == 0) {
+            throw EvaluatorException("No quantitative parameters found")
+        }
+        if (system.processes.size >= 1000) {
+            throw EvaluatorException("The current software version cannot perform the sensitivity analysis of a system with 1'000+ processes")
+        }
         val results = AnalysisProgram(
             system,
             targetProcess,
