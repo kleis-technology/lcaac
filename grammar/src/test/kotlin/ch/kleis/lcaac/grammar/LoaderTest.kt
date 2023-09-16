51ceb195294da578ea4d508640fc2470d0775496
Peva Blanchard <pblanchard@kleis.ch>
2023-09-16 09:38:59 +0200

    grammar: loader: tests: labels, products, allocation, reference unit closure

--- grammar/src/test/kotlin/ch/kleis/lcaac/grammar/LoaderTest.kt
+++ grammar/src/test/kotlin/ch/kleis/lcaac/grammar/LoaderTest.kt
@@ -1,5 +1,7 @@
 package ch.kleis.lcaac.grammar
 
+import ch.kleis.lcaac.core.lang.Register
+import ch.kleis.lcaac.core.lang.SymbolTable
 import ch.kleis.lcaac.core.lang.expression.*
 import ch.kleis.lcaac.core.math.basic.BasicNumber
 import ch.kleis.lcaac.core.math.basic.BasicOperations
@@ -9,7 +11,7 @@ import kotlin.test.assertEquals
 
 class LoaderTest {
     @Test
-    fun load_process() {
+    fun load_process_empty() {
         // given
         val file = lcaFile(
             """
@@ -26,6 +28,119 @@ class LoaderTest {
         val expected = EProcessTemplate<BasicNumber>(
             body = EProcess(
                 "p",
+            )
+        )
+        assertEquals(expected, actual.getTemplate("p"))
+    }
+
+    @Test
+    fun load_process_labels() {
+        // given
+        val file = lcaFile(
+            """
+                process p {
+                    labels {
+                        geo = "FR"
+                    }
+                }
+            """.trimIndent()
+        )
+        val loader = Loader(BasicOperations)
+
+        // when
+        val actual = loader.load(sequenceOf(file))
+
+        // then
+        val expected = EProcessTemplate<BasicNumber>(
+            body = EProcess(
+                "p",
+                labels = mapOf("geo" to EStringLiteral("FR")),
+            )
+        )
+        assertEquals(expected, actual.getTemplate("p", mapOf("geo" to "FR")))
+    }
+
+    @Test
+    fun load_process_products() {
+        // given
+        val file = lcaFile(
+            """
+                process p {
+                    products {
+                        1 kg out1 allocate 50 percent
+                        1 kg out2 allocate 50 percent
+                    }
+                }
+            """.trimIndent()
+        )
+        val oneKg = EQuantityScale(BasicNumber(1.0), EDataRef("kg"))
+        val fiftyPercent = EQuantityScale(BasicNumber(50.0), EDataRef("percent"))
+        val loader = Loader(BasicOperations)
+
+        // when
+        val actual = loader.load(sequenceOf(file))
+
+        // then
+        val oneKgClosed = EUnitOf(EQuantityClosure(SymbolTable.empty(), oneKg))
+        val expected = EProcessTemplate(
+            body = EProcess(
+                "p",
+                products = listOf(
+                    ETechnoExchange(oneKg, EProductSpec("out1", oneKgClosed), fiftyPercent),
+                    ETechnoExchange(oneKg, EProductSpec("out2", oneKgClosed), fiftyPercent),
+                )
+            )
+        )
+        assertEquals(expected, actual.getTemplate("p"))
+    }
+
+    @Test
+    fun load_process_products_withClosure() {
+        // given
+        val file = lcaFile(
+            """
+                variables {
+                    x = 1 kg
+                }
+                process p {
+                    variables {
+                        y = 1 kg
+                    }
+                    products {
+                        x + y out
+                    }
+                }
+            """.trimIndent()
+        )
+        val oneKg = EQuantityScale(BasicNumber(1.0), EDataRef("kg"))
+        val loader = Loader(BasicOperations)
+
+        // when
+        val actual = loader.load(sequenceOf(file))
+
+        // then
+        val sum = EQuantityAdd<BasicNumber>(EDataRef("x"), EDataRef("y"))
+        val localTable = SymbolTable(
+            data = Register.empty<DataExpression<BasicNumber>>().plus(
+                mapOf(
+                    "x" to oneKg,
+                    "y" to oneKg,
+                )
+            )
+        )
+        val referenceUnit = EUnitOf(EQuantityClosure(
+            localTable,
+            sum,
+            ))
+        val expected = EProcessTemplate(
+            locals = mapOf(
+                "y" to oneKg,
+            ),
+            body = EProcess(
+                "p",
+                products = listOf(
+                    ETechnoExchange(sum, EProductSpec("out", referenceUnit)),
+                )
             )
         )
         assertEquals(expected, actual.getTemplate("p"))
