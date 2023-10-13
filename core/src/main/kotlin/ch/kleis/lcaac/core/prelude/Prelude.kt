99f39d65392cd6b87dc47e7759b15226608be057
Peva Blanchard <pblanchard@kleis.ch>
2023-10-13 17:26:00 +0200

    core: prelude: introduced local function to ease reading

--- core/src/main/kotlin/ch/kleis/lcaac/core/prelude/Prelude.kt
+++ core/src/main/kotlin/ch/kleis/lcaac/core/prelude/Prelude.kt
@@ -30,8 +30,24 @@ class Prelude {
         }
     }
 
+
     companion object {
         const val PKG_NAME = "builtin_units"
+        private fun <Q> pud(symbol: String, dimension: Dimension): PrimitiveUnitDef<Q> =
+            PrimitiveUnitDef(
+                EUnitLiteral<Q>(UnitSymbol.of(symbol), 1.0, dimension)
+            )
+
+        private fun <Q> pud(symbol: String, dimension: String): PrimitiveUnitDef<Q> =
+            pud(symbol, Dimension.Companion.of(dimension))
+
+        private fun <Q> cud(
+            symbol: String,
+            scale: Double,
+            dimension: Dimension,
+            rawAlias: String
+        ): CompositeUnitDef<Q> =
+            CompositeUnitDef(EUnitLiteral(UnitSymbol.of(symbol), scale, dimension), rawAlias)
 
         // primitive dimensions
         private val mass = Dimension.of("mass")
@@ -42,107 +58,42 @@ class Prelude {
         private val radioactivity = Dimension.of("radioactivity")
         private val luminousIntensity = Dimension.of("luminous_intensity")
 
-        private fun <Q> molHpEq(): PrimitiveUnitDef<Q> = PrimitiveUnitDef(
-            EUnitLiteral(
-                UnitSymbol.of("mol H+-Eq"),
-                1.0,
-                Dimension.of("accumulated exceedance (AE)")
-            )
-        )
+        private fun <Q> molHpEq(): PrimitiveUnitDef<Q> = pud("mol H+-Eq", "accumulated exceedance (AE)")
         fun <Q> primitiveUnits(): Map<String, PrimitiveUnitDef<Q>> = listOf<PrimitiveUnitDef<Q>>(
-            PrimitiveUnitDef(EUnitLiteral(UnitSymbol.of("u"), 1.0, none)),
-            PrimitiveUnitDef(EUnitLiteral(UnitSymbol.of("kg"), 1.0, mass)),
-            PrimitiveUnitDef(EUnitLiteral(UnitSymbol.of("m"), 1.0, length)),
-            PrimitiveUnitDef(EUnitLiteral(UnitSymbol.of("K"), 1.0, temperature)),
-            PrimitiveUnitDef(EUnitLiteral(UnitSymbol.of("Wh"), 1.0, energy)),
-            PrimitiveUnitDef(EUnitLiteral(UnitSymbol.of("s"), 1.0, time)),
-            PrimitiveUnitDef(EUnitLiteral(UnitSymbol.of("Bq"), 1.0, radioactivity)),
-            PrimitiveUnitDef(EUnitLiteral(UnitSymbol.of("lumen"), 1.0, luminousIntensity)),
+            pud("u", none),
+            pud("kg", mass),
+            pud("m", length),
+            pud("K", temperature),
+            pud("Wh", energy),
+            pud("s", time),
+            pud("Bq", radioactivity),
+            pud("lumen", luminousIntensity),
             molHpEq(),
-            PrimitiveUnitDef(
-                EUnitLiteral(
-                    UnitSymbol.of("kg CO2-Eq"),
-                    1.0,
-                    Dimension.of("global warming potential (GWP100)")
-                )
-            ),
-            PrimitiveUnitDef(
-                EUnitLiteral(
-                    UnitSymbol.of("CTUe"),
-                    1.0,
-                    Dimension.of("comparative toxic unit for ecosystems (CTUe)")
-                )
-            ),
-            PrimitiveUnitDef(
-                EUnitLiteral(
-                    UnitSymbol.of("MJ, net calorific value"),
-                    1.0,
-                    Dimension.of("abiotic depletion potential (ADP): fossil fuels")
-                )
-            ),
-            PrimitiveUnitDef(
-                EUnitLiteral(
-                    UnitSymbol.of("kg P-Eq"),
-                    1.0,
-                    Dimension.of("fraction of nutrients reaching freshwater end compartment (P)")
-                )
-            ),
-            PrimitiveUnitDef(
-                EUnitLiteral(
-                    UnitSymbol.of("kg N-Eq"),
-                    1.0,
-                    Dimension.of("fraction of nutrients reaching marine end compartment (N)")
-                )
-            ),
-            PrimitiveUnitDef(
-                EUnitLiteral(
-                    UnitSymbol.of("CTUh"),
-                    1.0,
-                    Dimension.of("comparative toxic unit for human (CTUh)")
-                )
-            ),
-            PrimitiveUnitDef(
-                EUnitLiteral(
-                    UnitSymbol.of("kBq U235-Eq"),
-                    1.0,
-                    Dimension.of("human exposure efficiency relative to u235")
-                )
-            ),
-            PrimitiveUnitDef(
-                EUnitLiteral(
-                    UnitSymbol.of("kg Sb-Eq"),
-                    1.0,
-                    Dimension.of("abiotic depletion potential (ADP): elements (ultimate reserves)")
-                )
-            ),
-            PrimitiveUnitDef(
-                EUnitLiteral(
-                    UnitSymbol.of("kg CFC-11-Eq"),
-                    1.0,
-                    Dimension.of("ozone depletion potential (ODP)")
-                )
-            ),
-            PrimitiveUnitDef(
-                EUnitLiteral(
-                    UnitSymbol.of("disease incidence"),
-                    1.0,
-                    Dimension.of("impact on human health")
-                )
-            ),
-            PrimitiveUnitDef(
-                EUnitLiteral(
-                    UnitSymbol.of("kg NMVOC-Eq"),
-                    1.0,
-                    Dimension.of("tropospheric ozone concentration increase")
-                )
-            ),
-            PrimitiveUnitDef(
-                EUnitLiteral(
-                    UnitSymbol.of("m3 world eq. deprived"),
-                    1.0,
-                    Dimension.of("user deprivation potential (deprivation-weighted water consumption)"),
-                )
-            ),
+            pud("mol H+-Eq", "accumulated exceedance (AE)"),
+            pud("kg CO2-Eq", "global warming potential (GWP100)"),
+            pud("kg CO2-Eq", "global warming potential (GWP100)"),
+            pud("kg CO2-Eq", "global warming potential (GWP100)"),
+            pud("kg CO2-Eq", "global warming potential (GWP100)"),
+            pud("CTUe", "comparative toxic unit for ecosystems (CTUe)"),
+            pud("CTUe", "comparative toxic unit for ecosystems (CTUe)"),
+            pud("CTUe", "comparative toxic unit for ecosystems (CTUe)"),
+            pud("MJ, net calorific value", "abiotic depletion potential (ADP): fossil fuels"),
+            pud("kg P-Eq", "fraction of nutrients reaching freshwater end compartment (P)"),
+            pud("kg N-Eq", "fraction of nutrients reaching marine end compartment (N)"),
+            pud("mol N-Eq", "accumulated exceedance (AE)"),
+            pud("CTUh", "comparative toxic unit for human (CTUh)"),
+            pud("CTUh", "comparative toxic unit for human (CTUh)"),
+            pud("CTUh", "comparative toxic unit for human (CTUh)"),
+            pud("CTUh", "comparative toxic unit for human (CTUh)"),
+            pud("CTUh", "comparative toxic unit for human (CTUh)"),
+            pud("CTUh", "comparative toxic unit for human (CTUh)"),
+            pud("kBq U235-Eq", "human exposure efficiency relative to u235"),
+            pud("dimensionless", "soil quality index"),
+            pud("kg Sb-Eq", "abiotic depletion potential (ADP): elements (ultimate reserves)"),
+            pud("kg CFC-11-Eq", "ozone depletion potential (ODP)"),
+            pud("disease incidence", "impact on human health"),
+            pud("kg NMVOC-Eq", "tropospheric ozone concentration increase"),
+            pud("m3 world eq. deprived", "user deprivation potential (deprivation-weighted water consumption)"),
         ).associateBy { it.ref() }
 
         // composite dimensions
@@ -160,72 +111,70 @@ class Prelude {
 
         fun <Q> compositeUnits(): Map<String, CompositeUnitDef<Q>> = listOf<CompositeUnitDef<Q>>(
             // dimensionless
-            CompositeUnitDef(EUnitLiteral(UnitSymbol.of("dimensionless"), 1.0, none), "1 u"),
-            CompositeUnitDef(EUnitLiteral(UnitSymbol.of("piece"), 1.0, none), "1 u"),
-            CompositeUnitDef(EUnitLiteral(UnitSymbol.of("person"), 1.0, none), "1 u"),
-            CompositeUnitDef(EUnitLiteral(UnitSymbol.of("p"), 1.0, none), "1 u"),
-            CompositeUnitDef(EUnitLiteral(UnitSymbol.of("percent"), 1.0e-2, none), "1e-2 u"),
+            cud("dimensionless", 1.0, none, "1 u"),
+            cud("piece", 1.0, none, "1 u"),
+            cud("guest night", 1.0, none, "1 u"),
+            cud("person", 1.0, none, "1 u"),
+            cud("p", 1.0, none, "1 u"),
+            cud("percent", 1.0e-2, none, "1e-2 u"),
 
             // temperature
 
             // mass
-            CompositeUnitDef(EUnitLiteral(UnitSymbol.of("ton"), 1E3, mass), "1e3 kg"),
-            CompositeUnitDef(EUnitLiteral(UnitSymbol.of("g"), 1.0e-3, mass), "1e-3 kg"),
+            cud("ton", 1E3, mass, "1e3 kg"),
+            cud("g", 1.0e-3, mass, "1e-3 kg"),
 
             // length
-            CompositeUnitDef(EUnitLiteral(UnitSymbol.of("mm"), 1.0e-3, length), "1e-3 m"),
-            CompositeUnitDef(EUnitLiteral(UnitSymbol.of("cm"), 1.0e-2, length), "1e-2 m"),
-            CompositeUnitDef(EUnitLiteral(UnitSymbol.of("km"), 1.0e3, length), "1e3 m"),
+            cud("mm", 1.0e-3, length, "1e-3 m"),
+            cud("cm", 1.0e-2, length, "1e-2 m"),
+            cud("km", 1.0e3, length, "1e3 m"),
 
             // area
             CompositeUnitDef(EUnitLiteral(UnitSymbol.of("m2"), 1.0, area), "m^2"),
-            CompositeUnitDef(EUnitLiteral(UnitSymbol.of("ha"), 1.0e4, area), "1e4 m2"),
+            cud("ha", 1.0e4, area, "1e4 m2"),
             CompositeUnitDef(EUnitLiteral(UnitSymbol.of("km2"), 1.0e6, area), "1e6 m2"),
 
             // volume
             CompositeUnitDef(EUnitLiteral(UnitSymbol.of("m3"), 1.0, volume), "m^3"),
-            CompositeUnitDef(EUnitLiteral(UnitSymbol.of("l"), 1.0e-3, volume), "1e-3 m3"),
-            CompositeUnitDef(EUnitLiteral(UnitSymbol.of("cl"), 1.0e-5, volume), "1e-5 m3"),
-            CompositeUnitDef(EUnitLiteral(UnitSymbol.of("ml"), 1.0e-6, volume), "1e-6 m3"),
+            cud("l", 1.0e-3, volume, "1e-3 m3"),
+            cud("cl", 1.0e-5, volume, "1e-5 m3"),
+            cud("ml", 1.0e-6, volume, "1e-6 m3"),
 
             // radioactivity
-            CompositeUnitDef(EUnitLiteral(UnitSymbol.of("kBq"), 1000.0, radioactivity), "1e3 Bq"),
+            cud("kBq", 1000.0, radioactivity, "1e3 Bq"),
 
             // time
-            CompositeUnitDef(EUnitLiteral(UnitSymbol.of("min"), 60.0, time), "60 s"),
-            CompositeUnitDef(EUnitLiteral(UnitSymbol.of("hour"), 3600.0, time), "60 min"),
+            cud("min", 60.0, time, "60 s"),
+            cud("hour", 3600.0, time, "60 min"),
             CompositeUnitDef(EUnitLiteral(UnitSymbol.of("day"), 24 * 3600.0, time), "24 hour"),
             CompositeUnitDef(EUnitLiteral(UnitSymbol.of("year"), 365 * 24 * 3600.0, time), "365 day"),
 
             // energy
-            CompositeUnitDef(EUnitLiteral(UnitSymbol.of("kWh"), 1.0e3, energy), "1e3 Wh"),
-            CompositeUnitDef(EUnitLiteral(UnitSymbol.of("MWh"), 1.0e6, energy), "1e3 kWh"),
-            CompositeUnitDef(EUnitLiteral(UnitSymbol.of("J"), 1.0 / 3600.0, energy), "1 Wh / 3600 u"),
-            CompositeUnitDef(EUnitLiteral(UnitSymbol.of("kJ"), 1.0e3 / 3600.0, energy), "1e3 J"),
-            CompositeUnitDef(EUnitLiteral(UnitSymbol.of("MJ"), 1.0e6 / 3600.0, energy), "1e6 J"),
+            cud("kWh", 1.0e3, energy, "1e3 Wh"),
+            cud("MWh", 1.0e6, energy, "1e3 kWh"),
+            cud("J", 1.0/3600.0, energy, "1 Wh / 3600 u"),
+            cud("kJ", 1.0e3/3600.0, energy, "1e3 J"),
+            cud("MJ", 1.0e6/3600.0, energy, "1e6 J"),
 
             // power
-            CompositeUnitDef(EUnitLiteral(UnitSymbol.of("W"), 1.0 / 3600.0, power), "1 J / s"),
+            cud("W", 1.0/3600.0, power, "1 J / s"),
 
             // land_occupation
-            CompositeUnitDef(
-                EUnitLiteral(UnitSymbol.of("m2a"), 1.0 * 365 * 24 * 3600, land_occupation),
-                "1 m2 * 1 year"
-            ),
+            cud("m2a", 1.0 * 365 * 24 * 3600, land_occupation, "1 m2 * 1 year"),
 
             // illuminance
-            CompositeUnitDef(EUnitLiteral(UnitSymbol.of("lux"), 1.0, illuminance), "1 lumen / m2"),
+            cud("lux", 1.0, illuminance, "1 lumen / m2"),
 
             // other
-            CompositeUnitDef(EUnitLiteral(UnitSymbol.of("tkm"), 1e3 * 1e3, transport), "1 ton * km"),
-            CompositeUnitDef(EUnitLiteral(UnitSymbol.of("my"), 365 * 24 * 3600.0, length_time), "1 m * year"),
-            CompositeUnitDef(EUnitLiteral(UnitSymbol.of("personkm"), 1000.0, person_distance), "1 p * km"),
-            CompositeUnitDef(EUnitLiteral(UnitSymbol.of("kgy"), 365 * 24 * 3600.0, mass_time), "1 kg * year"),
-            CompositeUnitDef(EUnitLiteral(UnitSymbol.of("kga"), 365 * 24 * 3600.0, mass_time), "1 kg * year"),
-            CompositeUnitDef(EUnitLiteral(UnitSymbol.of("m3y"), 365 * 24 * 3600.0, volume_time), "1 m3 * year"),
+            cud("tkm", 1e3 * 1e3, transport, "1 ton * km"),
+            cud("my", 365 * 24 * 3600.0, length_time, "1 m * year"),
+            cud("personkm", 1000.0, person_distance, "1 p * km"),
+            cud("kgy", 365 * 24 * 3600.0, mass_time, "1 kg * year"),
+            cud("kga", 365 * 24 * 3600.0, mass_time, "1 kg * year"),
+            cud("m3y", 365 * 24 * 3600.0, volume_time, "1 m3 * year"),
 
             // accumulated exceedance
-            CompositeUnitDef(EUnitLiteral(UnitSymbol.of("mol N-Eq"), 1.0, Dimension.of("accumulated exceedance (AE)")), "1 ${molHpEq<Q>().ref()}"),
+            cud("mol N-Eq", 1.0, Dimension.of("accumulated exceedance (AE)"), "1 ${molHpEq<Q>().ref()}"),
         ).associateBy { it.ref() }
 
 
