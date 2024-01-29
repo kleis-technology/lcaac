d3cca6903684cd22fd7a954bfc3abe334b453d60
Peva Blanchard <pblanchard@kleis.ch>
2024-01-29 16:30:15 +0100

    cli: trace command

--- cli/src/main/kotlin/ch/kleis/lcaac/cli/Main.kt
+++ cli/src/main/kotlin/ch/kleis/lcaac/cli/Main.kt
@@ -3,11 +3,13 @@ package ch.kleis.lcaac.cli
 import ch.kleis.lcaac.cli.cmd.AssessCommand
 import ch.kleis.lcaac.cli.cmd.LcaacCommand
 import ch.kleis.lcaac.cli.cmd.TestCommand
+import ch.kleis.lcaac.cli.cmd.TraceCommand
 import com.github.ajalt.clikt.core.subcommands
 
 fun main(args: Array<String>) = LcaacCommand()
     .subcommands(
         AssessCommand(),
         TestCommand(),
+        TraceCommand(),
     )
     .main(args)
