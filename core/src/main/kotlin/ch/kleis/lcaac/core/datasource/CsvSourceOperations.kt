7623258944023b56127c2c98e1c67a37860e717a
Peva Blanchard <pblanchard@kleis.ch>
2024-05-07 16:40:45 +0200

    core: datasources: simplify classes

--- core/src/main/kotlin/ch/kleis/lcaac/core/datasource/CsvSourceOperations.kt
+++ core/src/main/kotlin/ch/kleis/lcaac/core/datasource/CsvSourceOperations.kt
@@ -1,13 +1,16 @@
 package ch.kleis.lcaac.core.datasource
 
 import ch.kleis.lcaac.core.lang.evaluator.EvaluatorException
-import ch.kleis.lcaac.core.lang.expression.DataExpression
-import ch.kleis.lcaac.core.lang.expression.EQuantityScale
-import ch.kleis.lcaac.core.lang.expression.ERecord
-import ch.kleis.lcaac.core.lang.expression.EStringLiteral
+import ch.kleis.lcaac.core.lang.evaluator.ToValue
+import ch.kleis.lcaac.core.lang.evaluator.reducer.DataExpressionReducer
+import ch.kleis.lcaac.core.lang.expression.*
+import ch.kleis.lcaac.core.lang.register.DataSourceRegister
+import ch.kleis.lcaac.core.lang.value.DataSourceValue
+import ch.kleis.lcaac.core.lang.value.DataValue
 import ch.kleis.lcaac.core.lang.value.QuantityValue
 import ch.kleis.lcaac.core.lang.value.StringValue
 import ch.kleis.lcaac.core.math.QuantityOperations
+import ch.kleis.lcaac.core.prelude.Prelude
 import org.apache.commons.csv.CSVFormat
 import org.apache.commons.csv.CSVParser
 import java.io.File
@@ -22,30 +25,76 @@ class CsvSourceOperations<Q>(
         val location = Paths.get(path.absolutePath, it)
         location.toFile().inputStream()
     }
-) : DataSourceOperationsBase<Q>(ops, { description ->
-    val inputStream = fileLoader(description.location)
-    val parser = CSVParser(inputStream.reader(), format)
-    val header = parser.headerMap
-    val schema = description.schema
-    parser.iterator().asSequence()
-        .map { csvRecord ->
-            val entries = header
-                .filter { entry -> schema.containsKey(entry.key) }
-                .mapValues { entry ->
-                    val columnDefaultValue = schema[entry.key]!!
-                    val position = entry.value
-                    val element = csvRecord[position]
-                    when (columnDefaultValue) {
-                        is QuantityValue -> parseQuantityWithDefaultUnit(ops, element, columnDefaultValue.unit.toEUnitLiteral())
-                        is StringValue -> EStringLiteral(element)
-                        else -> throw IllegalStateException(
-                            "invalid schema: column '${entry.key}' has an invalid default value"
-                        )
+) : DataSourceOperations<Q> {
+    private fun load(location: String, schema: Map<String, DataValue<Q>>): Sequence<ERecord<Q>> {
+        val inputStream = fileLoader(location)
+        val parser = CSVParser(inputStream.reader(), format)
+        val header = parser.headerMap
+        return parser.iterator().asSequence()
+            .map { csvRecord ->
+                val entries = header
+                    .filter { entry -> schema.containsKey(entry.key) }
+                    .mapValues { entry ->
+                        val columnDefaultValue = schema[entry.key]!!
+                        val position = entry.value
+                        val element = csvRecord[position]
+                        when (columnDefaultValue) {
+                            is QuantityValue -> parseQuantityWithDefaultUnit(ops, element, columnDefaultValue.unit.toEUnitLiteral())
+                            is StringValue -> EStringLiteral(element)
+                            else -> throw IllegalStateException(
+                                "invalid schema: column '${entry.key}' has an invalid default value"
+                            )
+                        }
                     }
+                ERecord(entries)
+            }
+    }
+
+    override fun readAll(source: DataSourceValue<Q>): Sequence<ERecord<Q>> {
+        val records = load(source.location, source.schema)
+        val filter = source.filter
+        return records
+            .filter { record ->
+                filter.entries.all {
+                    val expected = it.value
+                    if (expected is StringValue) {
+                        val actual = record.entries[it.key]
+                            ?.let { with(ToValue(ops)) { it.toValue() } }
+                            ?: throw IllegalStateException(
+                                "${source.location}: invalid schema: unknown column '${it.key}'"
+                            )
+                        actual == expected
+                    } else throw EvaluatorException("invalid matching condition $it")
                 }
-            ERecord(entries)
+            }
+    }
+
+    override fun sumProduct(source: DataSourceValue<Q>, columns: List<String>): DataExpression<Q> {
+        val reducer = DataExpressionReducer(
+            dataRegister = Prelude.units(),
+            dataSourceRegister = DataSourceRegister.empty(),
+            ops = ops,
+            sourceOps = this,
+        )
+        return readAll(source).map { record ->
+            columns.map { column ->
+                record.entries[column]
+                    ?: throw IllegalStateException(
+                        "${source.location}: invalid schema: unknown column '$column'"
+                    )
+            }.reduce { acc, expression ->
+                reducer.reduce(EQuantityMul(acc, expression))
+            }
+        }.reduce { acc, expression ->
+            reducer.reduce(EQuantityAdd(acc, expression))
         }
-})
+    }
+
+    override fun getFirst(source: DataSourceValue<Q>): ERecord<Q> {
+        return readAll(source).firstOrNull()
+            ?: throw EvaluatorException("no record found in '${source.location}' matching ${source.filter}")
+    }
+}
 
 private val format = CSVFormat.DEFAULT.builder()
     .setHeader()
--- core/src/main/kotlin/ch/kleis/lcaac/core/datasource/DataSourceDescription.kt
+++
@@ -1,8 +0,0 @@
-package ch.kleis.lcaac.core.datasource
-
-import ch.kleis.lcaac.core.lang.value.DataValue
-
-data class DataSourceDescription<Q>(
-    val location: String,
-    val schema: Map<String, DataValue<Q>>,
-)
