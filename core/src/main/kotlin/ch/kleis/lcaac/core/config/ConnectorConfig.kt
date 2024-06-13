02702e7755b5ceedb288beeeac55a6e4d3a0eeb9
Peva Blanchard <pblanchard@kleis.ch>
2024-06-13 22:06:55 +0200

    core: caching

--- core/src/main/kotlin/ch/kleis/lcaac/core/config/ConnectorConfig.kt
+++ core/src/main/kotlin/ch/kleis/lcaac/core/config/ConnectorConfig.kt
@@ -5,5 +5,12 @@ import kotlinx.serialization.Serializable
 @Serializable
 data class ConnectorConfig(
     val name: String,
+    val cache: CacheConfig,
     val options: Map<String, String>,
+)
+
+@Serializable
+data class CacheConfig(
+    val enabled: Boolean = false,
+    val maxSize: Long = 1024,
 )
