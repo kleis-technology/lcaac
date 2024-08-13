4ca2c532bdeaaab00fca511c757df1970a70020c
Peva Blanchard <pblanchard@kleis.ch>
2024-08-13 11:17:03 +0200

    core: removed comment

--- core/src/main/kotlin/ch/kleis/lcaac/core/datasource/DataSourceConnector.kt
+++ core/src/main/kotlin/ch/kleis/lcaac/core/datasource/DataSourceConnector.kt
@@ -5,15 +5,6 @@ import ch.kleis.lcaac.core.config.DataSourceConfig
 import ch.kleis.lcaac.core.lang.expression.*
 import ch.kleis.lcaac.core.lang.value.DataSourceValue
 
-/*
-    TODO:
-     - Move sumProduct back to DataSourceOperations
-     - Remove cached connector
-     - Caching at the level of data source operations
-     - InMemoryDatasource should have typed records (DataExpression<Q>)
-        - use reducer when computing sumProduct
- */
-
 interface DataSourceConnector<Q> {
     fun getName(): String
     fun getConfig(): ConnectorConfig
