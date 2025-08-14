7c8d654d79e0355b498f09fbb4aaa4d7529276f7
Sébastien Bros de Puechredon <sbros@kleis.ch>
2025-08-14 13:50:16 +0200

    rename test file

--- core/src/test/kotlin/ch/kleis/lcaac/core/lang/resolver/ProcessTemplateResolverTest.kt
+++ core/src/test/kotlin/ch/kleis/lcaac/core/lang/resolver/ProcessTemplateResolverTest.kt
@@ -11,7 +11,7 @@ import org.junit.jupiter.api.Test
 import kotlin.test.assertEquals
 import kotlin.test.assertFailsWith
 
-class ProcessResolverTest {
+class ProcessTemplateResolverTest {
     @Test
     fun resolve_withLabelMatching() {
         // given
