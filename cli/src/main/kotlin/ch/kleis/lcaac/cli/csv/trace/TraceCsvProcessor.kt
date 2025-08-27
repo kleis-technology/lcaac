42fb12032ecef58863000b7341bb2ce74a1305f3
Peva Blanchard <pblanchard@kleis.ch>
2025-08-27 13:49:09 +0200

    cli: trace csv processor

--- cli/src/main/kotlin/ch/kleis/lcaac/cli/csv/trace/TraceCsvProcessor.kt
+++ cli/src/main/kotlin/ch/kleis/lcaac/cli/csv/trace/TraceCsvProcessor.kt
@@ -1,12 +1,15 @@
 package ch.kleis.lcaac.cli.csv.trace
 
+import ch.kleis.lcaac.cli.cmd.prepareArguments
 import ch.kleis.lcaac.cli.csv.CsvRequest
+import ch.kleis.lcaac.core.assessment.ContributionAnalysisProgram
 import ch.kleis.lcaac.core.config.LcaacConfig
 import ch.kleis.lcaac.core.datasource.ConnectorFactory
 import ch.kleis.lcaac.core.datasource.DefaultDataSourceOperations
 import ch.kleis.lcaac.core.datasource.csv.CsvConnectorBuilder
 import ch.kleis.lcaac.core.lang.SymbolTable
 import ch.kleis.lcaac.core.lang.evaluator.Evaluator
+import ch.kleis.lcaac.core.lang.evaluator.EvaluatorException
 import ch.kleis.lcaac.core.lang.evaluator.reducer.DataExpressionReducer
 import ch.kleis.lcaac.core.math.basic.BasicNumber
 import ch.kleis.lcaac.core.math.basic.BasicOperations
@@ -28,7 +31,42 @@ class TraceCsvProcessor(
     private val dataReducer = DataExpressionReducer(symbolTable.data, symbolTable.dataSources, ops, sourceOps)
     private val evaluator = Evaluator(symbolTable, ops, sourceOps)
 
-    fun process(request: CsvRequest): List<TraceCsvResult> {
-        TODO()
+    fun process(request: CsvRequest): TraceCsvResult {
+        val reqName = request.processName
+        val reqLabels = request.matchLabels
+        val template = symbolTable.getTemplate(reqName, reqLabels)
+            ?: throw EvaluatorException("Could not get template for ${reqName}${reqLabels}")
+        val arguments = prepareArguments(dataReducer, template, request.toMap())
+        val trace = evaluator.trace(template, arguments)
+        val systemValue = trace.getSystemValue()
+        val entryPoint = trace.getEntryPoint()
+        val program = ContributionAnalysisProgram(systemValue, entryPoint)
+        val analysis = program.run()
+        val observablePorts = analysis.getObservablePorts()
+            .getElements()
+            .sortedWith(trace.getComparator())
+        val controllablePorts = analysis.getControllablePorts().getElements()
+            .sortedBy { it.getShortName() }
+        val products = entryPoint.products.asSequence()
+
+        val items = products.flatMap { demandedProduct ->
+            observablePorts.asSequence().map {
+                val depth = trace.getDepthOf(it)
+                val supply = analysis.supplyOf(it)
+                TraceCsvResultItem(
+                    depth ?: -1,
+                    demandedProduct,
+                    supply,
+                    it,
+                    controllablePorts.associateWith { col ->
+                        analysis.getPortContribution(it, col)
+                    },
+                )
+            }
+        }.toList()
+        return TraceCsvResult(
+            request,
+            trace = items,
+        )
     }
 }
